4a1c3bfdde39962da69a567a246f362492e48242
Matthew Wilcox <willy@infradead.org>
2017-12-04 04:02:00 -0500

    page cache: Remove stray radix comment
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2831,7 +2831,7 @@ repeat:
 			put_page(page);
 			if (err == -EEXIST)
 				goto repeat;
-			/* Presumably ENOMEM for radix tree node */
+			/* Presumably ENOMEM for xarray node */
 			return ERR_PTR(err);
 		}
 
