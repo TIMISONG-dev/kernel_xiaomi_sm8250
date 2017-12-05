5edff027501aaefe23f777bda8fb97b046221e80
Matthew Wilcox <willy@infradead.org>
2017-12-05 17:30:38 -0500

    pagevec: Use xa_mark_t
    Removes sparse warnings.
    
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -3083,7 +3083,7 @@ static int f2fs_write_cache_pages(struct address_space *mapping,
 	pgoff_t end;		/* Inclusive */
 	pgoff_t done_index;
 	int range_whole = 0;
-	int tag;
+	xa_mark_t tag;
 	int nwritten = 0;
 	int submitted = 0;
 	int i;
