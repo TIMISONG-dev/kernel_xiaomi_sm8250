29c9e2a2f4f996e541bbe323ffe8a1e31909bf0f
Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
2018-10-23 22:30:50 +0800

    blk-mq: place trace_block_getrq() in correct place
    trace_block_getrq() is to indicate a request struct has been allocated
    for queue, so put it in right place.
    
    Reviewed-by: Jianchao Wang <jianchao.w.wang@oracle.com>
    Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1942,8 +1942,6 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 
 	rq_qos_throttle(q, bio, NULL);
 
-	trace_block_getrq(q, bio, bio->bi_opf);
-
 	rq = blk_mq_get_request(q, bio, bio->bi_opf, &data);
 	if (unlikely(!rq)) {
 		rq_qos_cleanup(q, bio);
@@ -1951,6 +1949,8 @@ static blk_qc_t blk_mq_make_request(struct request_queue *q, struct bio *bio)
 			bio_wouldblock_error(bio);
 		return BLK_QC_T_NONE;
 	}
+
+	trace_block_getrq(q, bio, bio->bi_opf);
 
 	rq_qos_track(q, rq, bio);
 
