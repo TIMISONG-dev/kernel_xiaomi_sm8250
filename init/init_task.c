37dcfa5a566779798eb4087039993ca8c73c19df
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-02-28 10:40:39 +0530

    cpuset: Restore tasks affinity while moving across cpusets
    When tasks move across cpusets, the current affinity settings
    are lost. Cache the task affinity and restore it during cpuset
    migration. The restoring happens only when the cached affinity
    is subset of the current cpuset settings.
    
    Change-Id: I6c2ec1d5e3d994e176926d94b9e0cc92418020cc
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [satyap@codeaurora.org: fix trivial merge conflicts and
    replace cs->cpus_requested with cs->cpus_allowed]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    [Tashar02: Remove WALT guard and simplify retval check in core.c]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- init/init_task.c
+++ init/init_task.c
@@ -76,6 +76,7 @@ struct task_struct init_task
 	.cpus_mask	= CPU_MASK_ALL,
 	.max_allowed_capacity	= SCHED_CAPACITY_SCALE,
 	.nr_cpus_allowed= NR_CPUS,
+	.cpus_requested	= CPU_MASK_ALL,
 	.mm		= NULL,
 	.active_mm	= &init_mm,
 	.restart_block	= {
