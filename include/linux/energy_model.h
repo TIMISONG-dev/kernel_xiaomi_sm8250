263bd4aa3f5233cc0495cc9e65e0dce9e319b7f9
Quentin Perret <qperret@google.com>
2019-10-30 15:14:49 +0000

    PM / EM: Declare EM data types unconditionally
    The structs representing capacity states and performance domains of an
    Energy Model are currently only defined for CONFIG_ENERGY_MODEL=y. That
    makes it hard for code outside PM_EM to manipulate those structures
    without a lot of ifdefery or stubbed accessors.
    
    So, move the declaration of the two structs outside of the
    CONFIG_ENERGY_MODEL ifdef. The client code (e.g. EAS or thermal) always
    checks the return of em_cpu_get() before using it, so the exising code
    is still safe to use as-is.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20191030151451.7961-3-qperret@google.com
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/energy_model.h
+++ include/linux/energy_model.h
@@ -9,7 +9,6 @@
 #include <linux/sched/topology.h>
 #include <linux/types.h>
 
-#ifdef CONFIG_ENERGY_MODEL
 /**
  * em_cap_state - Capacity state of a performance domain
  * @frequency:	The CPU frequency in KHz, for consistency with CPUFreq
@@ -52,6 +51,7 @@ struct em_perf_domain {
 	unsigned long cpus[0];
 };
 
+#ifdef CONFIG_ENERGY_MODEL
 #define EM_CPU_MAX_POWER 0xFFFF
 
 /*
@@ -199,7 +199,6 @@ static inline int em_pd_nr_cap_states(struct em_perf_domain *pd)
 }
 
 #else
-struct em_perf_domain {};
 struct em_data_callback {};
 #define EM_DATA_CB(_active_power_cb) { }
 
