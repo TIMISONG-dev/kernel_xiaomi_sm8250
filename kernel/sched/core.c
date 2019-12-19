81c741a40959c0196ebc78219c65d28f73ead3a0
Qian Cai <cai@lca.pw>
2019-12-19 09:03:14 -0500

    sched/core: Remove unused variable from set_user_nice()
    This commit left behind an unused variable:
    
      5443a0be6121 ("sched: Use fair:prio_changed() instead of ad-hoc implementation") left behind an unused variable.
    
      kernel/sched/core.c: In function 'set_user_nice':
      kernel/sched/core.c:4507:16: warning: variable 'delta' set but not used
        int old_prio, delta;
                    ^~~~~
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: 5443a0be6121 ("sched: Use fair:prio_changed() instead of ad-hoc implementation")
    Link: https://lkml.kernel.org/r/20191219140314.1252-1-cai@lca.pw
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5016,7 +5016,7 @@ static inline int rt_effective_prio(struct task_struct *p, int prio)
 void set_user_nice(struct task_struct *p, long nice)
 {
 	bool queued, running;
-	int old_prio, delta;
+	int old_prio;
 	struct rq_flags rf;
 	struct rq *rq;
 
@@ -5050,7 +5050,6 @@ void set_user_nice(struct task_struct *p, long nice)
 	set_load_weight(p);
 	old_prio = p->prio;
 	p->prio = effective_prio(p);
-	delta = p->prio - old_prio;
 
 	if (queued)
 		enqueue_task(rq, p, ENQUEUE_RESTORE | ENQUEUE_NOCLOCK);
