4aebb3b7a4ee481ef44c22e99dc86847aef87892
Vishwanath Raju K <vishk@codeaurora.org>
2020-02-21 16:48:56 +0530

    staging/android/ion: Add video HEAP ID
    This new heap shall be used for ION heap allocations
    for video when s1 bypassed.
    
    Signed-off-by: Vishwanath Raju K <vishk@codeaurora.org>

--- drivers/staging/android/ion/ion.h
+++ drivers/staging/android/ion/ion.h
@@ -39,6 +39,7 @@
 #define ION_SECURE_DISPLAY_HEAP_NAME "secure_display"
 #define ION_AUDIO_HEAP_NAME    "audio"
 #define ION_CAMERA_HEAP_NAME   "camera"
+#define ION_VIDEO_HEAP_NAME    "video"
 
 #define ION_IS_CACHED(__flags)  ((__flags) & ION_FLAG_CACHED)
 
