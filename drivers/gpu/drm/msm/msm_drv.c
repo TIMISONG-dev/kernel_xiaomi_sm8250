4873340ca68c5653f1bb1b956309aebf4fad3d05
Peter Zijlstra <peterz@infradead.org>
2020-04-22 13:10:04 +0200

    sched: Remove sched_set_*() return value
    Ingo suggested that since the new sched_set_*() functions are
    implemented using the 'nocheck' variants, they really shouldn't ever
    fail, so remove the return value.
    
    Cc: axboe@kernel.dk
    Cc: daniel.lezcano@linaro.org
    Cc: sudeep.holla@arm.com
    Cc: airlied@redhat.com
    Cc: broonie@kernel.org
    Cc: paulmck@kernel.org
    Suggested-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -558,10 +558,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 			kthread_run(kthread_worker_fn,
 				&priv->disp_thread[i].worker,
 				"crtc_commit:%d", priv->disp_thread[i].crtc_id);
-		ret = sched_set_fifo(priv->disp_thread[i].thread);
-		if (ret)
-			pr_warn("display thread priority update failed: %d\n",
-									ret);
+		sched_set_fifo(priv->disp_thread[i].thread);
 
 		if (IS_ERR(priv->disp_thread[i].thread)) {
 			dev_err(dev, "failed to create crtc_commit kthread\n");
@@ -583,10 +580,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		 * frame_pending counters beyond 2. This can lead to commit
 		 * failure at crtc commit level.
 		 */
-		ret = sched_set_fifo(priv->event_thread[i].thread);
-		if (ret)
-			pr_warn("display event thread priority update failed: %d\n",
-									ret);
+		sched_set_fifo(priv->event_thread[i].thread);
 
 		if (IS_ERR(priv->event_thread[i].thread)) {
 			dev_err(dev, "failed to create crtc_event kthread\n");
