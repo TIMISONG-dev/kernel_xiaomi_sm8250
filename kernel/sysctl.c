5ec02c98eed5037aa3679e9db951197e4864a391
Christoph Hellwig <hch@lst.de>
2020-04-24 08:43:35 +0200

    mm: remove watermark_boost_factor_sysctl_handler
    watermark_boost_factor_sysctl_handler is just a pointless wrapper for
    proc_dointvec_minmax, so remove it and use proc_dointvec_minmax
    directly.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1608,7 +1608,7 @@ static struct ctl_table vm_table[] = {
 		.data		= &watermark_boost_factor,
 		.maxlen		= sizeof(watermark_boost_factor),
 		.mode		= 0644,
-		.proc_handler	= watermark_boost_factor_sysctl_handler,
+		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 	},
 #ifdef CONFIG_MULTIPLE_KSWAPD
