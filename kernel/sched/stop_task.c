ad1642a42bcd2840198c97e154a5ca02343b0abf
Dietmar Eggemann <dietmar.eggemann@arm.com>
2020-06-03 10:03:03 +0200

    sched/idle,stop: Remove .get_rr_interval from sched_class
    The idle task and stop task sched_classes return 0 in this function.
    
    The single call site in sched_rr_get_interval() calls
    p->sched_class->get_rr_interval() only conditional in case it is
    defined. Otherwise time_slice=0 will be used.
    
    The deadline sched class does not define it. Commit a57beec5d427
    ("sched: Make sched_class::get_rr_interval() optional") introduced
    the default time-slice=0 for sched classes which do not provide this
    function.
    
    So .get_rr_interval for idle and stop sched_class can be removed to
    shrink the code a little.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200603080304.16548-4-dietmar.eggemann@arm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/stop_task.c
+++ kernel/sched/stop_task.c
@@ -112,12 +112,6 @@ prio_changed_stop(struct rq *rq, struct task_struct *p, int oldprio)
 	BUG(); /* how!?, what priority? */
 }
 
-static unsigned int
-get_rr_interval_stop(struct rq *rq, struct task_struct *task)
-{
-	return 0;
-}
-
 static void update_curr_stop(struct rq *rq)
 {
 }
@@ -146,8 +140,6 @@ const struct sched_class stop_sched_class = {
 #endif
 
 	.task_tick		= task_tick_stop,
-
-	.get_rr_interval	= get_rr_interval_stop,
 
 	.prio_changed		= prio_changed_stop,
 	.switched_to		= switched_to_stop,
