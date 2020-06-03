80923679ebc283d98bfe9dffde14adfa2f930979
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-06-03 12:32:06 +0530

    Revert "kernel: time: Add delay after cpu_relax() in tight loops"
    This reverts commit 79447cdfb82730d656cb92ded0581419ca65a9a9.
    
    kerneltoast:
    "We have queued spin locks, so spin lock starvation isn't a problem."
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -178,7 +178,6 @@ struct hrtimer_clock_base *lock_hrtimer_base(const struct hrtimer *timer,
 			raw_spin_unlock_irqrestore(&base->cpu_base->lock, *flags);
 		}
 		cpu_relax();
-		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 
@@ -1240,7 +1239,6 @@ int hrtimer_cancel(struct hrtimer *timer)
 		if (ret >= 0)
 			return ret;
 		cpu_relax();
-		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 EXPORT_SYMBOL_GPL(hrtimer_cancel);
