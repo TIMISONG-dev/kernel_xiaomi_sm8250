2f3d1dbaa95aece26e20e51def5bcccb2468843c
Dietmar Eggemann <dietmar.eggemann@arm.com>
2020-06-03 10:03:02 +0200

    sched/core: Remove redundant 'preempt' param from sched_class->yield_to_task()
    Commit 6d1cafd8b56e ("sched: Resched proper CPU on yield_to()") moved
    the code to resched the CPU from yield_to_task_fair() to yield_to()
    making the preempt parameter in sched_class->yield_to_task()
    unnecessary. Remove it. No other sched_class implements yield_to_task().
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200603080304.16548-3-dietmar.eggemann@arm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1889,7 +1889,7 @@ struct sched_class {
 	void (*enqueue_task) (struct rq *rq, struct task_struct *p, int flags);
 	bool (*dequeue_task) (struct rq *rq, struct task_struct *p, int flags);
 	void (*yield_task)   (struct rq *rq);
-	bool (*yield_to_task)(struct rq *rq, struct task_struct *p, bool preempt);
+	bool (*yield_to_task)(struct rq *rq, struct task_struct *p);
 
 	void (*wakeup_preempt)(struct rq *rq, struct task_struct *p, int flags);
 
