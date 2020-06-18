5014353a662d350823d8a3bfc4e843e84c5eb034
fangqinwu <fangqinwu@xiaomi.com>
2020-06-18 07:18:03 +0200

    proc: some optimization for reclaim
    BSPSYS-10620
    
    This change includes:
     - fix reclaim count calculation issue
     - only reclaim un-shared pages
     - avoid burning CPU when nr_to_reclaim pages
     - export reclaim_pte_range() API for rtmm
    
    Change-Id: I04e77a2811f5ac4cae6d261c3eaaf88e26eee2ce
    Signed-off-by: fangqinwu <fangqinwu@xiaomi.com>
    (cherry picked from commit 7e67f10971240af85c0f1bee0ea38f639014261e)
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1775,7 +1775,7 @@ int reclaim_address_space(struct address_space *mapping,
 	return ret;
 }
 
-static int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
+int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
 				unsigned long end, struct mm_walk *walk)
 {
 	struct reclaim_param *rp = walk->private;
@@ -1800,6 +1800,12 @@ cont:
 
 		page = vm_normal_page(vma, addr, ptent);
 		if (!page)
+			continue;
+
+		if (!PageLRU(page))
+			continue;
+
+		if (page_mapcount(page) != 1)
 			continue;
 
 		if (isolate_lru_page(compound_head(page)))
@@ -1836,7 +1842,7 @@ cont:
 		goto cont;
 
 	cond_resched();
-	return 0;
+	return (rp->nr_to_reclaim == 0) ? -EPIPE : 0;
 }
 
 enum reclaim_type {
@@ -1926,6 +1932,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	const struct mm_walk_ops reclaim_walk_ops = {
 		.pmd_entry = reclaim_pte_range,
 	};
+	int ret;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -2011,9 +2018,11 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(mm, max(vma->vm_start, start),
+			ret = walk_page_range(mm, max(vma->vm_start, start),
 					min(vma->vm_end, end),
 					&reclaim_walk_ops, &rp);
+			if (ret)
+				break;
 			vma = vma->vm_next;
 		}
 	} else {
@@ -2031,8 +2040,10 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(mm, vma->vm_start, vma->vm_end,
+			ret = walk_page_range(mm, vma->vm_start, vma->vm_end,
 					&reclaim_walk_ops, &rp);
+			if (ret)
+				break;
 		}
 	}
 
