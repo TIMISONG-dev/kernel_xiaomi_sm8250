1f48e38231c22ddbf8bf005f78972ce15895e143
Isaac J. Manjarres <isaacm@codeaurora.org>
2020-07-10 17:48:15 -0700

    mm/oom_kill: Online memory before running oom-killer
    Attempt to online memory before invoking the oom-killer
    to alleviate the memory pressure in the system as quickly
    as possible, without sacrificing performance/user-experience
    by invoking the oom-killer.
    
    Change-Id: I0cf3c50fcd022cd282731bccb54bfbad45878bb1
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1096,6 +1096,13 @@ bool out_of_memory(struct oom_control *oc)
 
 	if (oom_killer_disabled)
 		return false;
+
+	if (try_online_one_block(numa_node_id())) {
+		/* Got some memory back */
+		WARN(1, "OOM killer had to online a memory block\n");
+		return true;
+	}
+
 #ifdef CONFIG_PRIORITIZE_OOM_TASKS
 	oc->min_kill_adj = OOM_SCORE_ADJ_MIN;
 #endif
@@ -1163,11 +1170,6 @@ bool out_of_memory(struct oom_control *oc)
 #endif
 
 	if (!oc->chosen) {
-		if (try_online_one_block(numa_node_id())) {
-			/* Got some memory back */
-			WARN(1, "OOM killer had to online a memory block\n");
-			return true;
-		}
 		check_panic_on_oom(oc);
 		select_bad_process(oc);
 	}
