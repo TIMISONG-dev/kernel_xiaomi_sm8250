342e804635df509b4185c60d9854acbbc319de26
J. Avila <elavila@google.com>
2020-07-14 22:03:38 +0000

    ANDROID: sched/rt: Add support for rt sync wakeups
    Some rt tasks undergo sync wakeup. Currently, these tasks will be placed
    on other, often sleeping or otherwise idle CPUs, which can lead to
    unnecessary power hits.
    
    Bug: 157906395
    Change-Id: I48864d0847bbe4f7813c842032880ad3f3b8b06b
    Signed-off-by: J. Avila <elavila@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1825,6 +1825,8 @@ extern const u32		sched_prio_to_wmult[40];
 #endif
 #define ENQUEUE_INITIAL		0x80
 
+#define ENQUEUE_WAKEUP_SYNC	0x80
+
 #define RETRY_TASK		((void *)-1UL)
 
 struct sched_class {
