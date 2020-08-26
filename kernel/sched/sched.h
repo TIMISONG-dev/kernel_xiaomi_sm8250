1f25ba7e05d94603d45b38eaeffab773de5049ac
Thomas Gleixner <tglx@linutronix.de>
2020-08-26 14:08:10 +0200

    sched/core: Make migrate disable and CPU hotplug cooperative
    On CPU unplug tasks which are in a migrate disabled region cannot be pushed
    to a different CPU until they returned to migrateable state.
    
    Account the number of tasks on a runqueue which are in a migrate disabled
    section and make the hotplug wait mechanism respect that.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102347.067278757@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1111,6 +1111,10 @@ struct rq {
 	struct cpuidle_state	*idle_state;
 	int			idle_state_idx;
 #endif
+
+#if defined(CONFIG_PREEMPT_RT) && defined(CONFIG_SMP)
+	unsigned int		nr_pinned;
+#endif
 };
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
