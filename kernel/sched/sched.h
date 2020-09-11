0f1100ca55a190835901e981e4d9ebbdcbfd7e48
Peter Zijlstra <peterz@infradead.org>
2020-09-11 09:54:27 +0200

    sched/hotplug: Ensure only per-cpu kthreads run during hotplug
    In preparation for migrate_disable(), make sure only per-cpu kthreads
    are allowed to run on !active CPUs.
    
    This is ran (as one of the very first steps) from the cpu-hotplug
    task which is a per-cpu kthread and completion of the hotplug
    operation only requires such tasks.
    
    This constraint enables the migrate_disable() implementation to wait
    for completion of all migrate_disable regions on this CPU at hotplug
    time without fear of any new ones starting.
    
    This replaces the unlikely(rq->balance_callbacks) test at the tail of
    context_switch with an unlikely(rq->balance_work), the fast path is
    not affected.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102346.292709163@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1034,6 +1034,7 @@ struct rq {
 	unsigned long		cpu_capacity;
 
 	struct callback_head	*balance_callback;
+	unsigned char		balance_flags;
 
 	unsigned char		nohz_idle_balance;
 	unsigned char		idle_balance;
@@ -1490,6 +1491,9 @@ init_numa_balancing(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_SMP
 
+#define BALANCE_WORK	0x01
+#define BALANCE_PUSH	0x02
+
 static inline void
 queue_balance_callback(struct rq *rq,
 		       struct callback_head *head,
@@ -1497,12 +1501,13 @@ queue_balance_callback(struct rq *rq,
 {
 	lockdep_assert_held(&rq->lock);
 
-	if (unlikely(head->next))
+	if (unlikely(head->next || (rq->balance_flags & BALANCE_PUSH)))
 		return;
 
 	head->func = (void (*)(struct callback_head *))func;
 	head->next = rq->balance_callback;
 	rq->balance_callback = head;
+	rq->balance_flags |= BALANCE_WORK;
 }
 
 #define rcu_dereference_check_sched_domain(p) \
