7d5b092b8218f821fa33b2f5bd753f3e75e64ecb
Peter Zijlstra <peterz@infradead.org>
2020-09-17 10:38:30 +0200

    sched: Add migrate_disable()
    Add the base migrate_disable() support (under protest).
    
    While migrate_disable() is (currently) required for PREEMPT_RT, it is
    also one of the biggest flaws in the system.
    
    Notably this is just the base implementation, it is broken vs
    sched_setaffinity() and hotplug, both solved in additional patches for
    ease of review.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102346.818170844@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2039,6 +2039,10 @@ static inline bool sched_fair_runnable(struct rq *rq)
 extern struct task_struct *pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf);
 extern struct task_struct *pick_next_task_idle(struct rq *rq);
 
+#define SCA_CHECK		0x01
+#define SCA_MIGRATE_DISABLE	0x02
+#define SCA_MIGRATE_ENABLE	0x04
+
 #ifdef CONFIG_SMP
 
 extern void update_group_capacity(struct sched_domain *sd, int cpu);
