d8e221e62e242b2ffa9ae95187ee2eb29db3e8f9
Valentin Schneider <valentin.schneider@arm.com>
2020-11-02 18:45:13 +0000

    sched/core: Remove select_task_rq()'s sd_flag parameter
    Only select_task_rq_fair() uses that parameter to do an actual domain
    search, other classes only care about what kind of wakeup is happening
    (fork, exec, or "regular") and thus just translate the flag into a wakeup
    type.
    
    WF_TTWU and WF_EXEC have just been added, use these along with WF_FORK to
    encode the wakeup types we care about. For select_task_rq_fair(), we can
    simply use the shiny new WF_flag : SD_flag mapping.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201102184514.2733-3-valentin.schneider@arm.com

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1623,12 +1623,12 @@ static void yield_task_dl(struct rq *rq)
 static int find_later_rq(struct task_struct *task);
 
 static int
-select_task_rq_dl(struct task_struct *p, int cpu, int sd_flag, int flags)
+select_task_rq_dl(struct task_struct *p, int cpu, int flags)
 {
 	struct task_struct *curr;
 	struct rq *rq;
 
-	if (sd_flag != SD_BALANCE_WAKE)
+	if (!(flags & WF_TTWU))
 		goto out;
 
 	rq = cpu_rq(cpu);
