f63012706bd00454314611199ed98d03fe7a02d0
Frederic Weisbecker <frederic@kernel.org>
2020-11-17 16:16:35 +0100

    sched: Detect call to schedule from critical entry code
    Detect calls to schedule() between user_enter() and user_exit(). Those
    are symptoms of early entry code that either forgot to protect a call
    to schedule() inside exception_enter()/exception_exit() or, in the case
    of HAVE_CONTEXT_TRACKING_OFFSTACK, enabled interrupts or preemption in
    a wrong spot.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201117151637.259084-4-frederic@kernel.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4866,6 +4866,7 @@ static inline void schedule_debug(struct task_struct *prev)
 		preempt_count_set(PREEMPT_DISABLED);
 	}
 	rcu_sleep_check();
+	SCHED_WARN_ON(ct_state() == CONTEXT_USER);
 
 	profile_hit(SCHED_PROFILING, __builtin_return_address(0));
 
