0bec7ca5cc5944e83d981918c76b344a33aeb8aa
Peter Zijlstra <peterz@infradead.org>
2020-11-17 18:19:43 -0500

    sched: Trivial forced-newidle balancer
    When a sibling is forced-idle to match the core-cookie; search for
    matching tasks to fill the core.
    
    rcu_read_unlock() can incur an infrequent deadlock in
    sched_core_balance(). Fix this by using the RCU-sched flavor instead.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Tested-by: Hongyu Ning <hongyu.ning@linux.intel.com>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210422123308.800048269@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1233,6 +1233,8 @@ static inline raw_spinlock_t *__rq_lockp(struct rq *rq)
 
 bool cfs_prio_less(struct task_struct *a, struct task_struct *b, bool fi);
 
+extern void queue_core_balance(struct rq *rq);
+
 #else /* !CONFIG_SCHED_CORE */
 
 static inline bool sched_core_enabled(struct rq *rq)
@@ -1253,6 +1255,10 @@ static inline raw_spinlock_t *rq_lockp(struct rq *rq)
 static inline raw_spinlock_t *__rq_lockp(struct rq *rq)
 {
 	return &rq->__lock;
+}
+
+static inline void queue_core_balance(struct rq *rq)
+{
 }
 
 #endif /* CONFIG_SCHED_CORE */
