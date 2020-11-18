6738d953f9120369f4bac0228620e8749d796252
Thomas Gleixner <tglx@linutronix.de>
2020-11-18 20:48:42 +0100

    sched: Make migrate_disable/enable() independent of RT
    Now that the scheduler can deal with migrate disable properly, there is no
    real compelling reason to make it only available for RT.
    
    There are quite some code pathes which needlessly disable preemption in
    order to prevent migration and some constructs like kmap_atomic() enforce
    it implicitly.
    
    Making it available independent of RT allows to provide a preemptible
    variant of kmap_atomic() and makes the code more consistent in general.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Grudgingly-Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20201118204007.269943012@linutronix.de
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1118,7 +1118,7 @@ struct rq {
 	int			idle_state_idx;
 #endif
 
-#if defined(CONFIG_PREEMPT_RT) && defined(CONFIG_SMP)
+#ifdef CONFIG_SMP
 	unsigned int		nr_pinned;
 #endif
 	unsigned int		push_busy;
@@ -1154,7 +1154,7 @@ static inline int cpu_of(struct rq *rq)
 
 static inline bool is_migration_disabled(struct task_struct *p)
 {
-#if defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT)
+#ifdef CONFIG_SMP
 	return p->migration_disabled;
 #else
 	return false;
