be9f708006c0dacf4186d84faaf75a091359a9c9
Mel Gorman <mgorman@techsingularity.net>
2020-11-20 09:06:28 +0000

    sched/fair: Avoid unnecessary calculation of load imbalance at clone time
    In find_idlest_group(), the load imbalance is only relevant when the group
    is either overloaded or fully busy but it is calculated unconditionally.
    This patch moves the imbalance calculation to the context it is required.
    Technically, it is a micro-optimisation but really the benefit is avoiding
    confusing one type of imbalance with another depending on the group_type
    in the next patch.
    
    No functional change.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20201120090630.3286-3-mgorman@techsingularity.net

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8956,9 +8956,6 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 			.group_type = group_overloaded,
 	};
 
-	imbalance = scale_load_down(NICE_0_LOAD) *
-				(sd->imbalance_pct-100) / 100;
-
 	do {
 		int local_group;
 
@@ -9012,6 +9009,11 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 	switch (local_sgs.group_type) {
 	case group_overloaded:
 	case group_fully_busy:
+
+		/* Calculate allowed imbalance based on load */
+		imbalance = scale_load_down(NICE_0_LOAD) *
+				(sd->imbalance_pct-100) / 100;
+
 		/*
 		 * When comparing groups across NUMA domains, it's possible for
 		 * the local domain to be very lightly loaded relative to the
