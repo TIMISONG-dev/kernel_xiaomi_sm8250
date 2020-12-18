e556c5197885ac7e9de466c2481c7e5e2fe41865
Xuewen Yan <xuewen.yan@unisoc.com>
2020-12-18 17:27:52 +0800

    sched/fair: Avoid stale CPU util_est value for schedutil in task dequeue
    [ Upstream commit 8c1f560c1ea3f19e22ba356f62680d9d449c9ec2 ]
    
    CPU (root cfs_rq) estimated utilization (util_est) is currently used in
    dequeue_task_fair() to drive frequency selection before it is updated.
    
    with:
    
    CPU_util        : rq->cfs.avg.util_avg
    CPU_util_est    : rq->cfs.avg.util_est
    CPU_utilization : max(CPU_util, CPU_util_est)
    task_util       : p->se.avg.util_avg
    task_util_est   : p->se.avg.util_est
    
    dequeue_task_fair():
    
        /* (1) CPU_util and task_util update + inform schedutil about
               CPU_utilization changes */
        for_each_sched_entity() /* 2 loops */
            (dequeue_entity() ->) update_load_avg() -> cfs_rq_util_change()
             -> cpufreq_update_util() ->...-> sugov_update_[shared\|single]
             -> sugov_get_util() -> cpu_util_cfs()
    
        /* (2) CPU_util_est and task_util_est update */
        util_est_dequeue()
    
    cpu_util_cfs() uses CPU_utilization which could lead to a false (too
    high) utilization value for schedutil in task ramp-down or ramp-up
    scenarios during task dequeue.
    
    To mitigate the issue split the util_est update (2) into:
    
     (A) CPU_util_est update in util_est_dequeue()
     (B) task_util_est update in util_est_update()
    
    Place (A) before (1) and keep (B) where (2) is. The latter is necessary
    since (B) relies on task_util update in (1).
    
    Fixes: 7f65ea42eb00 ("sched/fair: Add util_est on top of PELT")
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/1608283672-18240-1-git-send-email-xuewen.yan94@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3832,6 +3832,24 @@ static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 
 #define UTIL_EST_MARGIN (SCHED_CAPACITY_SCALE / 100)
 
+static inline void util_est_dequeue(struct cfs_rq *cfs_rq,
+				    struct task_struct *p)
+{
+	unsigned int enqueued;
+
+	if (!sched_feat(UTIL_EST))
+		return;
+
+	/* Update root cfs_rq's estimated utilization */
+	enqueued  = cfs_rq->avg.util_est.enqueued;
+	enqueued -= min_t(unsigned int, enqueued,
+			  (_task_util_est(p) | UTIL_AVG_UNCHANGED));
+	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, enqueued);
+
+	/* Update plots for CPU's estimated utilization */
+	trace_sched_util_est_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
+}
+
 /*
  * Check if a (signed) value is within a specified (unsigned) margin,
  * based on the observation that:
@@ -3845,24 +3863,15 @@ static inline bool within_margin(int value, int margin)
 	return ((unsigned int)(value + margin - 1) < (2 * margin - 1));
 }
 
-static void
-util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
+static inline void util_est_update(struct cfs_rq *cfs_rq,
+				   struct task_struct *p,
+				   bool task_sleep)
 {
 	long last_ewma_diff, last_enqueued_diff;
 	struct util_est ue;
-	int cpu;
 
 	if (!sched_feat(UTIL_EST))
 		return;
-
-	/* Update root cfs_rq's estimated utilization */
-	ue.enqueued  = cfs_rq->avg.util_est.enqueued;
-	ue.enqueued -= min_t(unsigned int, ue.enqueued,
-			     (_task_util_est(p) | UTIL_AVG_UNCHANGED));
-	WRITE_ONCE(cfs_rq->avg.util_est.enqueued, ue.enqueued);
-
-	/* Update plots for CPU's estimated utilization */
-	trace_sched_util_est_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 
 	/*
 	 * Skip update of task's estimated utilization when the task has not
@@ -3910,8 +3919,7 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 	 * To avoid overestimation of actual task utilization, skip updates if
 	 * we cannot grant there is idle time in this CPU.
 	 */
-	cpu = cpu_of(rq_of(cfs_rq));
-	if (task_util(p) > capacity_orig_of(cpu))
+	if (task_util(p) > capacity_orig_of(cpu_of(rq_of(cfs_rq))))
 		return;
 
 	/*
@@ -4107,8 +4115,11 @@ static inline void
 util_est_enqueue(struct cfs_rq *cfs_rq, struct task_struct *p) {}
 
 static inline void
-util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p,
-		 bool task_sleep) {}
+util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p) {}
+
+static inline void
+util_est_update(struct cfs_rq *cfs_rq, struct task_struct *p,
+		bool task_sleep) {}
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq) {}
 
 #endif /* CONFIG_SMP */
@@ -5664,6 +5675,8 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
 
+	util_est_dequeue(&rq->cfs, p);
+
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
 	 * the cfs_rq utilization to select a frequency.
@@ -5719,7 +5732,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		dec_rq_walt_stats(rq, p);
 	}
 
-	util_est_dequeue(&rq->cfs, p, task_sleep);
+	util_est_update(&rq->cfs, p, task_sleep);
 	hrtick_update(rq);
 }
 
