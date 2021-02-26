f3539c48a7120d7254b41b01b5f8072705b160e9
Miaohe Lin <linmiaohe@huawei.com>
2021-02-25 17:18:27 -0800

    BACKPORT: mm/zsmalloc.c: convert to use kmem_cache_zalloc in cache_alloc_zspage()
    We always memset the zspage allocated via cache_alloc_zspage.  So it's
    more convenient to use kmem_cache_zalloc in cache_alloc_zspage than caller
    do it manually.
    
    Link: https://lkml.kernel.org/r/20210114120032.25885-1-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1086,9 +1086,7 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 	if (!zspage)
 		return NULL;
 
-	if (!IS_ENABLED(CONFIG_COMPACTION))
-		gfp &= ~__GFP_MOVABLE;
-
+	memset(zspage, 0, sizeof(struct zspage));
 	zspage->magic = ZSPAGE_MAGIC;
 	migrate_lock_init(zspage);
 
