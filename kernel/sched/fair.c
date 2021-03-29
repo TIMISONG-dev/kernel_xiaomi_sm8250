d7156235d062d1a8bdd1d076b85820c773fcb094
YueHaibing <yuehaibing@huawei.com>
2021-03-29 22:40:29 +0800

    sched/fair: Move update_nohz_stats() to the CONFIG_NO_HZ_COMMON block to simplify the code & fix an unused function warning
    When !CONFIG_NO_HZ_COMMON we get this new GCC warning:
    
       kernel/sched/fair.c:8398:13: warning: ‘update_nohz_stats’ defined but not used [-Wunused-function]
    
    Move update_nohz_stats() to an already existing CONFIG_NO_HZ_COMMON #ifdef
    block.
    
    Beyond fixing the GCC warning, this also simplifies the update_nohz_stats() function.
    
    [ mingo: Rewrote the changelog. ]
    
    Fixes: 0826530de3cb ("sched/fair: Remove update of blocked load from newidle_balance")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20210329144029.29200-1-yuehaibing@huawei.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8847,28 +8847,6 @@ group_type group_classify(unsigned int imbalance_pct,
 	return group_has_spare;
 }
 
-static bool update_nohz_stats(struct rq *rq)
-{
-#ifdef CONFIG_NO_HZ_COMMON
-	unsigned int cpu = rq->cpu;
-
-	if (!rq->has_blocked_load)
-		return false;
-
-	if (!cpumask_test_cpu(cpu, nohz.idle_cpus_mask))
-		return false;
-
-	if (!time_after(jiffies, READ_ONCE(rq->last_blocked_load_update_tick)))
-		return true;
-
-	update_blocked_averages(cpu);
-
-	return rq->has_blocked_load;
-#else
-	return false;
-#endif
-}
-
 static inline bool
 sched_reduced_capacity(struct rq *rq, struct sched_domain *sd)
 {
@@ -10960,6 +10938,24 @@ out:
 	 * enable the periodic update of the load of idle cpus
 	 */
 	WRITE_ONCE(nohz.has_blocked, 1);
+}
+
+static bool update_nohz_stats(struct rq *rq)
+{
+	unsigned int cpu = rq->cpu;
+
+	if (!rq->has_blocked_load)
+		return false;
+
+	if (!cpumask_test_cpu(cpu, nohz.idle_cpus_mask))
+		return false;
+
+	if (!time_after(jiffies, READ_ONCE(rq->last_blocked_load_update_tick)))
+		return true;
+
+	update_blocked_averages(cpu);
+
+	return rq->has_blocked_load;
 }
 
 /*
