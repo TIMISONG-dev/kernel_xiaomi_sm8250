78361d634d03b71254e398d14cf66cbdde6bac8f
Alexey Dobriyan <adobriyan@gmail.com>
2021-04-22 23:02:28 +0300

    sched: Make multiple runqueue task counters 32-bit
    Make:
    
    	struct dl_rq::dl_nr_migratory
    	struct dl_rq::dl_nr_running
    
    	struct rt_rq::rt_nr_boosted
    	struct rt_rq::rt_nr_migratory
    	struct rt_rq::rt_nr_total
    
    	struct rq::nr_uninterruptible
    
    32-bit.
    
    If total number of tasks can't exceed 2**32 (and less due to futex pid
    limits), then per-runqueue counters can't as well.
    
    This patchset has been sponsored by REX Prefix Eradication Society.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20210422200228.1423391-4-adobriyan@gmail.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -690,8 +690,8 @@ struct rt_rq {
 	} highest_prio;
 #endif
 #ifdef CONFIG_SMP
-	unsigned long		rt_nr_migratory;
-	unsigned long		rt_nr_total;
+	unsigned int		rt_nr_migratory;
+	unsigned int		rt_nr_total;
 	int			overloaded;
 	struct plist_head	pushable_tasks;
 
@@ -705,7 +705,7 @@ struct rt_rq {
 	raw_spinlock_t		rt_runtime_lock;
 
 #ifdef CONFIG_RT_GROUP_SCHED
-	unsigned long		rt_nr_boosted;
+	unsigned int		rt_nr_boosted;
 
 	struct rq		*rq;
 	struct task_group	*tg;
@@ -722,7 +722,7 @@ struct dl_rq {
 	/* runqueue is an rbtree, ordered by deadline */
 	struct rb_root_cached	root;
 
-	unsigned long		dl_nr_running;
+	unsigned int		dl_nr_running;
 
 #ifdef CONFIG_SMP
 	/*
@@ -736,7 +736,7 @@ struct dl_rq {
 		u64		next;
 	} earliest_dl;
 
-	unsigned long		dl_nr_migratory;
+	unsigned int		dl_nr_migratory;
 	int			overloaded;
 
 	/*
@@ -1023,7 +1023,7 @@ struct rq {
 	 * one CPU and if it got migrated afterwards it may decrease
 	 * it on another CPU. Always updated under the runqueue lock:
 	 */
-	unsigned long		nr_uninterruptible;
+	unsigned int		nr_uninterruptible;
 
 	struct task_struct	*curr;
 	struct task_struct	*idle;
