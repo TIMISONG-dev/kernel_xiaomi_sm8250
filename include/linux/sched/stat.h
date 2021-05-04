8566d36a07e9acf07ad7354cf2df4ca8613b89a2
Peter Zijlstra <peterz@infradead.org>
2021-05-04 22:43:42 +0200

    sched: Simplify sched_info_on()
    The situation around sched_info is somewhat complicated, it is used by
    sched_stats and delayacct and, indirectly, kvm.
    
    If SCHEDSTATS=Y (but disabled by default) sched_info_on() is
    unconditionally true -- this is the case for all distro kernel configs
    I checked.
    
    If for some reason SCHEDSTATS=N, but TASK_DELAY_ACCT=Y, then
    sched_info_on() can return false when delayacct is disabled,
    presumably because there would be no other users left; except kvm is.
    
    Instead of complicating matters further by accurately accounting
    sched_stat and kvm state, simply unconditionally enable when
    SCHED_INFO=Y, matching the common distro case.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lkml.kernel.org/r/20210505111525.121458839@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/sched/stat.h
+++ include/linux/sched/stat.h
@@ -3,6 +3,7 @@
 #define _LINUX_SCHED_STAT_H
 
 #include <linux/percpu.h>
+#include <linux/kconfig.h>
 
 /*
  * Various counters maintained by the scheduler and fork(),
@@ -38,14 +39,7 @@ static inline u64 sched_lpm_disallowed_time(int cpu)
 
 static inline int sched_info_on(void)
 {
-#ifdef CONFIG_SCHEDSTATS
-	return 1;
-#elif defined(CONFIG_TASK_DELAY_ACCT)
-	extern int delayacct_on;
-	return delayacct_on;
-#else
-	return 0;
-#endif
+	return IS_ENABLED(CONFIG_SCHED_INFO);
 }
 
 #ifdef CONFIG_SCHEDSTATS
