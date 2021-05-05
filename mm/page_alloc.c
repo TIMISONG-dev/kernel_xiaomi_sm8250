40572d2a51e5ab8e15ae72b7a2bee7d52b12d81f
Minchan Kim <minchan@kernel.org>
2021-05-04 18:37:34 -0700

    mm: use proper type for cma_[alloc|release]
    size_t in cma_alloc is confusing since it makes people think it's byte
    count, not pages.  Change it to unsigned long[1].
    
    The unsigned int in cma_release is also not right so change it.  Since we
    have unsigned long in cma_release, free_contig_range should also respect
    it.
    
    [1] 67a2e213e7e9, mm: cma: fix incorrect type conversion for size during dma allocation
    
    Link: https://lore.kernel.org/linux-mm/20210324043434.GP1719932@casper.infradead.org/
    Link: https://lkml.kernel.org/r/20210331164018.710560-1-minchan@kernel.org
    Change-Id: Iabac44d6fbdb3604b9a6d02f1f9f1e2c4b950f03
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8623,9 +8623,9 @@ done:
 	return ret;
 }
 
-void free_contig_range(unsigned long pfn, unsigned nr_pages)
+void free_contig_range(unsigned long pfn, unsigned long nr_pages)
 {
-	unsigned int count = 0;
+	unsigned long count = 0;
 
 	for (; nr_pages--; pfn++) {
 		struct page *page = pfn_to_page(pfn);
@@ -8633,7 +8633,7 @@ void free_contig_range(unsigned long pfn, unsigned nr_pages)
 		count += page_count(page) != 1;
 		__free_page(page);
 	}
-	WARN(count != 0, "%d pages are still in use!\n", count);
+	WARN(count != 0, "%lu pages are still in use!\n", count);
 }
 #endif
 
