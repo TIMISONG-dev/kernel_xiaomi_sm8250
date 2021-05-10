8a5952bce02e66cbccc10e9e6758cdfbe069caee
Peter Zijlstra <peterz@infradead.org>
2021-05-10 14:01:00 +0200

    delayacct: Add sysctl to enable at runtime
    Just like sched_schedstats, allow runtime enabling (and disabling) of
    delayacct. This is useful if one forgot to add the delayacct boot time
    option.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/YJkhebGJAywaZowX@hirez.programming.kicks-ass.net
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -69,6 +69,7 @@
 #include <linux/mount.h>
 #include <linux/pipe_fs_i.h>
 #include <linux/userfaultfd_k.h>
+#include <linux/delayacct.h>
 
 #include "../lib/kstrtox.h"
 
@@ -515,6 +516,17 @@ static struct ctl_table kern_table[] = {
 	},
 #endif /* CONFIG_SCHEDSTATS */
 #endif /* CONFIG_SMP */
+#ifdef CONFIG_TASK_DELAY_ACCT
+	{
+		.procname	= "task_delayacct",
+		.data		= NULL,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sysctl_delayacct,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+#endif /* CONFIG_TASK_DELAY_ACCT */
 #ifdef CONFIG_NUMA_BALANCING
 	{
 		.procname	= "numa_balancing_scan_delay_ms",
