7dd1e3ed32fe10d9c9672cbc649a0e80ecb68e8c
Xuewen Yan <xuewen.yan@unisoc.com>
2021-06-10 19:17:36 +0800

    sched: avoid stable bucket value when task dequeue
    [root cause]: when bucket value would keep unchanged when task is not 0
    [changes]: check tasks when the bucket's tasks is not 0
    [side effects]: no
    [self test]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: sharkl5pro
    [test case]: scheduler
    [reviewers]: ke.wang
    [change_type]: bringup --> sprd_chip_support
    [tag_product]: common
    
    Now when task dequeue, consider following scenarios:
    
    The bucket->tasks != 0, task->uclamp_value = bucket->value
    
    Because task has been dequeued, the task's uclamp_min/max should
    no longer work, but in fact, because the bucket->tasks != 0, the
    bucket->value would keep unchanged.
    
    As a result, the rq->value would keep unchanged, futher more,
    when change frequency in schedutil, the rq->value would may cause
    the cpu get bigger frequency, and would affect power consumption.
    
    The patch check tasks in bucket, when task dequeued, and update
    the bucket->value if there is no other tasks which uclamp is
    equal to the dequeued task.
    
    Bug: 1551433
    Change-Id: I68d10916f3265f3d5e902d9c1c2189dfbf126d3f
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -792,6 +792,12 @@ static inline long se_runnable(struct sched_entity *se)
 
 #endif
 
+#ifdef CONFIG_RT_GROUP_SCHED
+#define rt_entity_is_task(rt_se) (!(rt_se)->my_q)
+#else /* CONFIG_RT_GROUP_SCHED */
+#define rt_entity_is_task(rt_se) (1)
+#endif /* CONFIG_RT_GROUP_SCHED */
+
 #ifdef CONFIG_SMP
 /*
  * XXX we want to get rid of these helpers and use the full load resolution.
