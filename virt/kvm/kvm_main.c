fada25b3f884c3b1ff61a63b82d0616e89a21067
Peter Zijlstra <peterz@infradead.org>
2021-06-11 10:28:13 +0200

    sched,perf,kvm: Fix preemption condition
    When ran from the sched-out path (preempt_notifier or perf_event),
    p->state is irrelevant to determine preemption. You can get preempted
    with !task_is_running() just fine.
    
    The right indicator for preemption is if the task is still on the
    runqueue in the sched-out path.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20210611082838.285099381@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4256,7 +4256,7 @@ static void kvm_sched_out(struct preempt_notifier *pn,
 {
 	struct kvm_vcpu *vcpu = preempt_notifier_to_vcpu(pn);
 
-	if (current->state == TASK_RUNNING)
+        if (current->on_rq)
 		vcpu->preempted = true;
 	kvm_arch_vcpu_put(vcpu);
 }
