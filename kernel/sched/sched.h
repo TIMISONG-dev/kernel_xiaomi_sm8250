9c23d60f7ba5fd6303e115cc410cfd22766c8f91
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2021-09-10 18:18:15 -0700

    sched/topology: Introduce sched_group::flags
    There exist situations in which the load balance needs to know the
    properties of the CPUs in a scheduling group. When using asymmetric
    packing, for instance, the load balancer needs to know not only the
    state of dst_cpu but also of its SMT siblings, if any.
    
    Use the flags of the child scheduling domains to initialize scheduling
    group flags. This will reflect the properties of the CPUs in the
    group.
    
    A subsequent changeset will make use of these new flags. No functional
    changes are introduced.
    
    Originally-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Reviewed-by: Len Brown <len.brown@intel.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210911011819.12184-3-ricardo.neri-calderon@linux.intel.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1881,6 +1881,7 @@ struct sched_group {
 	unsigned int		cores;
 	struct sched_group_capacity *sgc;
 	int			asym_prefer_cpu;	/* CPU of highest priority in group */
+	int			flags;
 
 	/*
 	 * The CPUs this group covers.
