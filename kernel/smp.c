412a4ec94a30477262d29f1680428a9881a61b29
Peter Zijlstra <peterz@infradead.org>
2021-09-21 22:16:02 +0200

    sched: Simplify wake_up_*idle*()
    Simplify and make wake_up_if_idle() more robust, also don't iterate
    the whole machine with preempt_disable() in it's caller:
    wake_up_all_idle_cpus().
    
    This prepares for another wake_up_if_idle() user that needs a full
    do_idle() cycle.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Vasily Gorbik <gor@linux.ibm.com>
    Tested-by: Vasily Gorbik <gor@linux.ibm.com> # on s390
    Link: https://lkml.kernel.org/r/20210929152428.769328779@infradead.org
    Change-Id: If9f0dd5d99dd675828656161bfdba299f447f998
    (cherry picked from 8850cb663b5cda04d33f9cfbc38889d73d3c8e24)
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/smp.c
+++ kernel/smp.c
@@ -869,12 +869,13 @@ void wake_up_all_idle_cpus(void)
 {
 	int cpu;
 
-	preempt_disable();
+	cpus_read_lock();
 	for_each_online_cpu(cpu) {
-		if (cpu == smp_processor_id())
-			wake_up_if_idle(cpu);
+		if (cpu == raw_smp_processor_id())
+			continue;
+		wake_up_if_idle(cpu);
 	}
-	preempt_enable();
+	cpus_read_unlock();
 }
 EXPORT_SYMBOL_GPL(wake_up_all_idle_cpus);
 
