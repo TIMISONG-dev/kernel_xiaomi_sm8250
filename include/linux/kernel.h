530aaaa53a61da545c14e7cd2991506cda5e4bb7
Thomas Gleixner <tglx@linutronix.de>
2021-09-23 18:54:35 +0200

    sched: Clean up the might_sleep() underscore zoo
    __might_sleep() vs. ___might_sleep() is hard to distinguish. Aside of that
    the three underscore variant is exposed to provide a checkpoint for
    rescheduling points which are distinct from blocking points.
    
    They are semantically a preemption point which means that scheduling is
    state preserving. A real blocking operation, e.g. mutex_lock(), wait*(),
    which cannot preserve a task state which is not equal to RUNNING.
    
    While technically blocking on a "sleeping" spinlock in RT enabled kernels
    falls into the voluntary scheduling category because it has to wait until
    the contended spin/rw lock becomes available, the RT lock substitution code
    can semantically be mapped to a voluntary preemption because the RT lock
    substitution code and the scheduler are providing mechanisms to preserve
    the task state and to take regular non-lock related wakeups into account.
    
    Rename ___might_sleep() to __might_resched() to make the distinction of
    these functions clear.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210923165357.928693482@linutronix.de
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -218,7 +218,7 @@ extern int _cond_resched(void);
 #endif
 
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
-extern void ___might_sleep(const char *file, int line, int preempt_offset);
+extern void __might_resched(const char *file, int line, int preempt_offset);
 extern void __might_sleep(const char *file, int line, int preempt_offset);
 extern void __cant_sleep(const char *file, int line, int preempt_offset);
 extern void __cant_migrate(const char *file, int line);
@@ -256,8 +256,8 @@ extern void __cant_migrate(const char *file, int line);
 	} while (0)
 
 #else
-  static inline void ___might_sleep(const char *file, int line,
-				   int preempt_offset) { }
+  static inline void __might_resched(const char *file, int line,
+				     int preempt_offset) { }
   static inline void __might_sleep(const char *file, int line,
 				   int preempt_offset) { }
 # define might_sleep() do { might_resched(); } while (0)
