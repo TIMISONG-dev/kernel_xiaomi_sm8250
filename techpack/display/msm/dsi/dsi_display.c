3f64751fc9cbdb2a8c7715f563429b429fe81b90
EcrosoftXiao <xy19990903@gmail.com>
2021-11-05 11:26:12 +0800

    techpack: display: Make dimlayer exposure switchable
    [Ayrton: Adapt to non fod devices, define sysfs attributes for switch]

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -245,7 +245,7 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 	}
 
 #ifdef CONFIG_DRM_SDE_EXPO
-	if (bl_lvl && !panel->spec_pdata->aod_mode) {
+	if(panel->dimlayer_exposure) {
 		bl_temp = expo_map_dim_level((u32)bl_temp, dsi_display);
 	}
 #endif
