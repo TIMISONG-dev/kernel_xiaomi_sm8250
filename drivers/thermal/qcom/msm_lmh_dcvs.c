88ff95a671bf0fd03d293cd764faeb48bc5ad40d
Pavel Dubrova <pashadubrova@gmail.com>
2021-12-07 04:02:32 +0200

    thermal: msm_lmh_dcvs: Protect lmh_enabled with qcom,legacy-lmh-enable
    Initialize LMH TZ only once only on legacy targets with defined
    qcom,legacy-lmh-enable property.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/thermal/qcom/msm_lmh_dcvs.c
+++ drivers/thermal/qcom/msm_lmh_dcvs.c
@@ -345,7 +345,7 @@ static struct limits_dcvs_hw *get_dcvsh_hw_from_cpu(int cpu)
 	return NULL;
 }
 
-static int enable_lmh(void)
+static int enable_lmh(struct device_node *dn)
 {
 	int ret = 0;
 	struct scm_desc desc_arg;
@@ -362,7 +362,8 @@ static int enable_lmh(void)
 		return ret;
 	}
 
-	lmh_enabled = true;
+	if (of_property_read_bool(dn, "qcom,legacy-lmh-enable"))
+		lmh_enabled = true;
 
 	return ret;
 }
@@ -655,7 +656,7 @@ static int limits_dcvs_probe(struct platform_device *pdev)
 				affinity);
 			return ret;
 		}
-		ret = enable_lmh();
+		ret = enable_lmh(dn);
 		if (ret)
 			return ret;
 	}
