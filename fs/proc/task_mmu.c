b29f3157aa6b7d19a8c6aa0bdb782550e37f14e3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-04-26 21:57:01 +0530

    Revert "mm: Enhance per process reclaim to consider shared pages"
    This reverts commit 5a83f94ad7cac48dc445aa8f62e380835397ff7e.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1823,7 +1823,7 @@ cont:
 			break;
 	}
 	pte_unmap_unlock(pte - 1, ptl);
-	reclaimed = reclaim_pages_from_list(&page_list, vma);
+	reclaimed = reclaim_pages_from_list(&page_list);
 	rp->nr_reclaimed += reclaimed;
 	rp->nr_to_reclaim -= reclaimed;
 	if (rp->nr_to_reclaim < 0)
