7eb337fcafeebbac1b6a0255c915546351a8c6ac
Bing Huang <huangbing@kylinos.cn>
2022-07-23 05:36:09 +0800

    sched/fair: Make per-cpu cpumasks static
    The load_balance_mask and select_rq_mask percpu variables are only used in
    kernel/sched/fair.c.
    
    Make them static and move their allocation into init_sched_fair_class().
    
    Replace kzalloc_node() with zalloc_cpumask_var_node() to get rid of the
    CONFIG_CPUMASK_OFFSTACK #ifdef and to align with per-cpu cpumask
    allocation for RT (local_cpu_mask in init_sched_rt_class()) and DL
    class (local_cpu_mask_dl in init_sched_dl_class()).
    
    [ mingo: Tidied up changelog & touched up the code. ]
    
    Signed-off-by: Bing Huang <huangbing@kylinos.cn>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20220722213609.3901-1-huangbing775@126.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6456,8 +6456,8 @@ static bool dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 #ifdef CONFIG_SMP
 
 /* Working cpumask for: load_balance, load_balance_newidle. */
-DEFINE_PER_CPU(cpumask_var_t, load_balance_mask);
-DEFINE_PER_CPU(cpumask_var_t, select_rq_mask);
+static DEFINE_PER_CPU(cpumask_var_t, load_balance_mask);
+static DEFINE_PER_CPU(cpumask_var_t, select_rq_mask);
 
 #ifdef CONFIG_NO_HZ_COMMON
 
@@ -12719,6 +12719,13 @@ void show_numa_stats(struct task_struct *p, struct seq_file *m)
 __init void init_sched_fair_class(void)
 {
 #ifdef CONFIG_SMP
+	int i;
+
+	for_each_possible_cpu(i) {
+		zalloc_cpumask_var_node(&per_cpu(load_balance_mask, i), GFP_KERNEL, cpu_to_node(i));
+		zalloc_cpumask_var_node(&per_cpu(select_rq_mask,    i), GFP_KERNEL, cpu_to_node(i));
+	}
+
 	open_softirq(SCHED_SOFTIRQ, run_rebalance_domains);
 
 #ifdef CONFIG_NO_HZ_COMMON
