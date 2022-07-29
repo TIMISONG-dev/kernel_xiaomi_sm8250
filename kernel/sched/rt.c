11e8b01e1de748245657b8f079de967457701b9d
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-07-29 13:13:03 +0200

    sched/core: Introduce sched_asym_cpucap_active()
    Create an inline helper for conditional code to be only executed on
    asymmetric CPU capacity systems. This makes these (currently ~10 and
    future) conditions a lot more readable.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20220729111305.1275158-2-dietmar.eggemann@arm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -460,7 +460,7 @@ static inline bool rt_task_fits_capacity(struct task_struct *p, int cpu)
 	unsigned int cpu_cap;
 
 	/* Only heterogeneous systems can benefit from this check */
-	if (!static_branch_unlikely(&sched_asym_cpucapacity))
+	if (!sched_asym_cpucap_active())
 		return true;
 
 	min_cap = uclamp_eff_value(p, UCLAMP_MIN);
@@ -1794,7 +1794,7 @@ static int find_lowest_rq(struct task_struct *task)
 	 * lowest_mask.
 	 */
 	if (IS_ENABLED(CONFIG_RT_SOFTIRQ_AWARE_SCHED) ||
-	    static_branch_unlikely(&sched_asym_cpucapacity)) {
+	    sched_asym_cpucap_active()) {
 
 		ret = cpupri_find_fitness(&task_rq(task)->rd->cpupri,
 					  task, lowest_mask,
