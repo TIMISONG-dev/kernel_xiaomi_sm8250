5820ca6838a0f08adec2b5a3792b4ae8e732f045
Chengming Zhou <zhouchengming@bytedance.com>
2022-08-18 20:48:03 +0800

    sched/fair: Allow changing cgroup of new forked task
    commit 7dc603c9028e ("sched/fair: Fix PELT integrity for new tasks")
    introduce a TASK_NEW state and an unnessary limitation that would fail
    when changing cgroup of new forked task.
    
    Because at that time, we can't handle task_change_group_fair() for new
    forked fair task which hasn't been woken up by wake_up_new_task(),
    which will cause detach on an unattached task sched_avg problem.
    
    This patch delete this unnessary limitation by adding check before do
    detach or attach in task_change_group_fair().
    
    So cpu_cgrp_subsys.can_attach() has nothing to do for fair tasks,
    only define it in #ifdef CONFIG_RT_GROUP_SCHED.
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20220818124805.601-8-zhouchengming@bytedance.com
    
    [ Use p->state still. -- Juansheng ]

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11516,6 +11516,13 @@ void init_cfs_rq(struct cfs_rq *cfs_rq)
 #ifdef CONFIG_FAIR_GROUP_SCHED
 static void task_change_group_fair(struct task_struct *p)
 {
+	/*
+	 * We couldn't detach or attach a forked task which
+	 * hasn't been woken up by wake_up_new_task().
+	 */
+	if (p->state == TASK_NEW)
+		return;
+
 	detach_task_cfs_rq(p);
 
 #ifdef CONFIG_SMP
