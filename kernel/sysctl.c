3dd5215469b9ea12e25ee0abf5bbe23b578f7a91
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-29 19:23:57 +0530

    sysctl: Allow maximum vm swappiness upto 200
    Change-Id: Ie0bb854db5834e2e2292682b927c5dcd8b5aaaac
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -134,6 +134,7 @@ static int __maybe_unused four = 4;
 static unsigned long zero_ul;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
+static int two_hundred = 200;
 static int one_thousand = 1000;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
@@ -1738,7 +1739,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &one_hundred,
+		.extra2		= &two_hundred,
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
