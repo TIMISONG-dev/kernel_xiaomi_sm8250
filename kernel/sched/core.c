5941b9726d4b9a089c4abeeb714860a06726b68c
Hao Jia <jiahao.os@bytedance.com>
2023-06-13 16:20:10 +0800

    sched/core: Avoid double calling update_rq_clock() in __balance_push_cpu_stop()
    There is a double update_rq_clock() invocation:
    
      __balance_push_cpu_stop()
        update_rq_clock()
        __migrate_task()
          update_rq_clock()
    
    Sadly select_fallback_rq() also needs update_rq_clock() for
    __do_set_cpus_allowed(), it is not possible to remove the update from
    __balance_push_cpu_stop(). So remove it from __migrate_task() and
    ensure all callers of this function call update_rq_clock() prior to
    calling it.
    
    Signed-off-by: Hao Jia <jiahao.os@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20230613082012.49615-3-jiahao.os@bytedance.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2419,7 +2419,6 @@ static struct rq *__migrate_task(struct rq *rq, struct rq_flags *rf,
 	if (!is_cpu_allowed(p, dest_cpu))
 		return rq;
 
-	update_rq_clock(rq);
 	rq = move_queued_task(rq, rf, p, dest_cpu);
 
 	return rq;
@@ -2477,10 +2476,12 @@ static int migration_cpu_stop(void *data)
 				goto out;
 		}
 
-		if (task_on_rq_queued(p))
+		if (task_on_rq_queued(p)) {
+			update_rq_clock(rq);
 			rq = __migrate_task(rq, &rf, p, arg->dest_cpu);
-		else
+		} else {
 			p->wake_cpu = arg->dest_cpu;
+		}
 
 		/*
 		 * XXX __migrate_task() can fail, at which point we might end
