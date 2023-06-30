2700907b14186e3d5b4516c693982400f93d8dbf
Suren Baghdasaryan <surenb@google.com>
2023-06-29 17:56:12 -0700

    sched/psi: use kernfs polling functions for PSI trigger polling
    [ Upstream commit aff037078ecaecf34a7c2afab1341815f90fba5e ]
    
    Destroying psi trigger in cgroup_file_release causes UAF issues when
    a cgroup is removed from under a polling process. This is happening
    because cgroup removal causes a call to cgroup_file_release while the
    actual file is still alive. Destroying the trigger at this point would
    also destroy its waitqueue head and if there is still a polling process
    on that file accessing the waitqueue, it will step on the freed pointer:
    
    do_select
      vfs_poll
                               do_rmdir
                                 cgroup_rmdir
                                   kernfs_drain_open_files
                                     cgroup_file_release
                                       cgroup_pressure_release
                                         psi_trigger_destroy
                                           wake_up_pollfree(&t->event_wait)
    // vfs_poll is unblocked
                                           synchronize_rcu
                                           kfree(t)
      poll_freewait -> UAF access to the trigger's waitqueue head
    
    Patch [1] fixed this issue for epoll() case using wake_up_pollfree(),
    however the same issue exists for synchronous poll() case.
    The root cause of this issue is that the lifecycles of the psi trigger's
    waitqueue and of the file associated with the trigger are different. Fix
    this by using kernfs_generic_poll function when polling on cgroup-specific
    psi triggers. It internally uses kernfs_open_node->poll waitqueue head
    with its lifecycle tied to the file's lifecycle. This also renders the
    fix in [1] obsolete, so revert it.
    
    [1] commit c2dbe32d5db5 ("sched/psi: Fix use-after-free in ep_remove_wait_queue()")
    
    Fixes: 0e94682b73bf ("psi: introduce psi monitor")
    Closes: https://lore.kernel.org/all/20230613062306.101831-1-lujialin4@huawei.com/
    Reported-by: Lu Jialin <lujialin4@huawei.com>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20230630005612.1014540-1-surenb@google.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/psi.h
+++ include/linux/psi.h
@@ -21,8 +21,9 @@ void psi_memstall_leave(unsigned long *flags);
 
 int psi_show(struct seq_file *s, struct psi_group *group, enum psi_res res);
 
-struct psi_trigger *psi_trigger_create(struct psi_group *group,
-			char *buf, enum psi_res res, struct file *file);
+struct psi_trigger *psi_trigger_create(struct psi_group *group, char *buf,
+				       enum psi_res res, struct file *file,
+				       struct kernfs_open_file *of);
 void psi_trigger_destroy(struct psi_trigger *t);
 
 __poll_t psi_trigger_poll(void **trigger_ptr, struct file *file,
