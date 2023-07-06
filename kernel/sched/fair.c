269a3757dc9111d904e4b64497b11462cb580fbb
Vincent Guittot <vincent.guittot@linaro.org>
2023-07-06 15:51:44 +0200

    sched/fair: Remove util_est boosting
    There is no need to use runnable_avg when estimating util_est and that
    even generates wrong behavior because one includes blocked tasks whereas
    the other one doesn't. This can lead to accounting twice the waking task p,
    once with the blocked runnable_avg and another one when adding its
    util_est.
    
    cpu's runnable_avg is already used when computing util_avg which is then
    compared with util_est.
    
    In some situation, feec will not select prev_cpu but another one on the
    same performance domain because of higher max_util
    
    Fixes: 7d0583cf9ec7 ("sched/fair, cpufreq: Introduce 'runnable boosting'")
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20230706135144.324311-1-vincent.guittot@linaro.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6434,9 +6434,6 @@ cpu_util(int cpu, struct task_struct *p, int dst_cpu, int boost)
 		unsigned long util_est;
 		util_est = READ_ONCE(cfs_rq->avg.util_est);
 
-		if (boost)
-			util_est = max(util_est, runnable);
-
 		/*
 		 * During wake-up @p isn't enqueued yet and doesn't contribute
 		 * to any cpu_rq(cpu)->cfs.avg.util_est.enqueued.
