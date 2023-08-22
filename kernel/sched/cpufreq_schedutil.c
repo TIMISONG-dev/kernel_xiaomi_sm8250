cb63b47712453ec545268f0d43e190ec5e6a8670
Juansheng <jaycopter@foxmail.com>
2023-08-23 00:31:06 +0800

    sched/sugov: Ignore busy filter when rq is capped by uclamp_max
    Refs:
    [1] https://github.com/sm8250-mainline/linux/commit/7a17e1db
    [2] https://github.com/torvalds/linux/commit/7a17e1db
    
    sugov_update_single_{freq, perf}() contains a 'busy' filter that ensures
    we don't bring the frqeuency down if there's no idle time (CPU is busy).
    
    The problem is that with uclamp_max we will have scenarios where a busy
    task is capped to run at a lower frequency and this filter prevents
    applying the capping when this task starts running.
    
    We handle this by skipping the filter when uclamp is enabled and the rq
    is being capped by uclamp_max.
    
    We introduce a new function uclamp_rq_is_capped() to help detecting when
    this capping is taking effect. Some code shuffling was required to allow
    using cpu_util_{cfs, rt}() in this new function.
    
    On 2 Core SMT2 Intel laptop I see:
    
    Without this patch:
    
    	uclampset -M 0 sysbench --test=cpu --threads = 4 run
    
    produces a score of ~3200 consistently. Which is the highest possible.
    
    Compiling the kernel also results in frequency running at max 3.1GHz all
    the time - running uclampset -M 400 to cap it has no effect without this
    patch.
    
    With this patch:
    
    	uclampset -M 0 sysbench --test=cpu --threads = 4 run
    
    produces a score of ~1100 with some outliers in ~1700. Uclamp max
    aggregates the performance requirements, so having high values sometimes
    is expected if some other task happens to require that frequency starts
    running at the same time.
    
    When compiling the kernel with uclampset -M 400 I can see the
    frequencies mostly in the ~2GHz region. Helpful to conserve power and
    prevent heating when not plugged in.
    
    Fixes: 982d9cd ("sched/cpufreq, sched/uclamp: Add clamps for FAIR and RT tasks")
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20211216225320.2957053-2-qais.yousef@arm.com

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -528,8 +528,11 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	/*
 	 * Do not reduce the frequency if the CPU has not been idle
 	 * recently, as the reduction is likely to be premature then.
+	 *
+	 * Except when the rq is capped by uclamp_max.
 	 */
-	if (busy && next_f < sg_policy->next_freq) {
+	if (!uclamp_rq_is_capped(cpu_rq(sg_cpu->cpu)) &&
+	    busy && next_f < sg_policy->next_freq) {
 		next_f = sg_policy->next_freq;
 
 		/* Restore cached freq as next_freq has changed */
