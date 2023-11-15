2b534fa06d5ec6235d8f7c301357fb45794524cf
Abel Wu <wuyun.abel@bytedance.com>
2023-11-15 11:36:45 +0800

    sched/fair: eevdf: Sort the rbtree by virtual deadline
    Sort the task timeline by virtual deadline and keep the min_vruntime
    in the augmented tree, so we can avoid doubling the worst case cost
    and make full use of the cached leftmost node to enable O(1) fastpath
    picking in next patch.
    
    Signed-off-by: Abel Wu <wuyun.abel@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20231115033647.80785-3-wuyun.abel@bytedance.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -519,7 +519,7 @@ struct sched_entity {
 	struct load_weight		load;
 	struct rb_node			run_node;
 	u64				deadline;
-	u64				min_deadline;
+	u64				min_vruntime;
 
 	struct list_head		group_node;
 	unsigned int			on_rq;
