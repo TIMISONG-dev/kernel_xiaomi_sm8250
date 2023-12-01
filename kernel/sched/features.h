f55fb15422e45f0cdd69246111d41541b2f4a11d
Vincent Guittot <vincent.guittot@linaro.org>
2023-12-01 17:16:51 +0100

    sched/fair: Remove SCHED_FEAT(UTIL_EST_FASTUP, true)
    sched_feat(UTIL_EST_FASTUP) has been added to easily disable the feature
    in order to check for possibly related regressions. After 3 years, it has
    never been used and no regression has been reported. Let's remove it
    and make fast increase a permanent behavior.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Hongyan Xia <hongyan.xia2@arm.com>
    Reviewed-by: Tang Yizhou <yizhou.tang@shopee.com>
    Reviewed-by: Yanteng Si <siyanteng@loongson.cn> [for the Chinese translation]
    Reviewed-by: Alex Shi <alexs@kernel.org>
    Link: https://lore.kernel.org/r/20231201161652.1241695-2-vincent.guittot@linaro.org

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -89,7 +89,6 @@ SCHED_FEAT(WA_BIAS, true)
  * UtilEstimation. Use estimated CPU utilization.
  */
 SCHED_FEAT(UTIL_EST, true)
-SCHED_FEAT(UTIL_EST_FASTUP, true)
 
 /*
  * Fast pre-selection of CPU candidates for EAS.
