e7cab07404f381c5f2590ddf30046205323d7c37
Ingo Molnar <mingo@kernel.org>
2024-03-08 11:58:54 +0100

    BACKPORT: sched/balancing: Change 'enum cpu_idle_type' to have more natural definitions
    The cpu_idle_type enum has the confusingly inverted property
    that 'not idle' is 1, and 'idle' is '0'.
    
    This resulted in a number of unnecessary complications in the code.
    
    Reverse the order, remove the CPU_NOT_IDLE type, and convert
    all code to a natural boolean form.
    
    It's much more readable:
    
      -       enum cpu_idle_type idle = this_rq->idle_balance ?
      -                                               CPU_IDLE : CPU_NOT_IDLE;
      -
      +       enum cpu_idle_type idle = this_rq->idle_balance;
    
      --------------------------------
    
      -       if (env->idle == CPU_NOT_IDLE || !busiest->sum_nr_running)
      +       if (!env->idle || !busiest->sum_nr_running)
    
      --------------------------------
    
    And gets rid of the double negation in these usages:
    
      -               if (env->idle != CPU_NOT_IDLE && env->src_rq->nr_running <= 1)
      +               if (env->idle && env->src_rq->nr_running <= 1)
    
    Furthermore, this makes code much more obvious where there's
    differentiation between CPU_IDLE and CPU_NEWLY_IDLE.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: "Gautham R. Shenoy" <gautham.shenoy@amd.com>
    Link: https://lore.kernel.org/r/20240308105901.1096078-4-mingo@kernel.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9084,7 +9084,7 @@ static int detach_tasks(struct lb_env *env)
 		 * We don't want to steal all, otherwise we may be treated likewise,
 		 * which could at worst lead to a livelock crash.
 		 */
-		if (env->idle != CPU_NOT_IDLE && env->src_rq->nr_running <= 1)
+		if (env->idle && env->src_rq->nr_running <= 1)
 			break;
 
 		env->loop++;
@@ -9901,8 +9901,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 				sgs->group_misfit_task_load = rq->misfit_task_load;
 				*sg_status |= SG_OVERLOAD;
 			}
-		} else if ((env->idle != CPU_NOT_IDLE) &&
-			   sched_reduced_capacity(rq, env->sd)) {
+		} else if (env->idle && sched_reduced_capacity(rq, env->sd)) {
 			/* Check for a task running on a CPU with reduced capacity */
 			if (sgs->group_misfit_task_load < load)
 				sgs->group_misfit_task_load = load;
@@ -9915,7 +9914,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 
 	/* Check if dst CPU is idle and preferred to this group */
 	if (!local_group && env->sd->flags & SD_ASYM_PACKING &&
-	    env->idle != CPU_NOT_IDLE && sgs->sum_h_nr_running &&
+	    env->idle && sgs->sum_h_nr_running &&
 	    sched_asym(env, sds, sgs, group)) {
 		sgs->group_asym_packing = 1;
 	}
@@ -10623,7 +10622,7 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 			 * waiting task in this overloaded busiest group. Let's
 			 * try to pull it.
 			 */
-			if (env->idle != CPU_NOT_IDLE && env->imbalance == 0) {
+			if (env->idle && env->imbalance == 0) {
 				env->migration_type = migrate_task;
 				env->imbalance = 1;
 			}
@@ -10837,7 +10836,7 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 		goto force_balance;
 
 	if (busiest->group_type != group_overloaded) {
-		if (env->idle == CPU_NOT_IDLE)
+		if (!env->idle)
 			/*
 			 * If the busiest group is not overloaded (and as a
 			 * result the local one too) but this CPU is already
@@ -11032,7 +11031,7 @@ asym_active_balance(struct lb_env *env)
 	 * lower priority CPUs in order to pack all tasks in the
 	 * highest priority CPUs.
 	 */
-	return env->idle != CPU_NOT_IDLE && (env->sd->flags & SD_ASYM_PACKING) &&
+	return env->idle && (env->sd->flags & SD_ASYM_PACKING) &&
 	       sched_asym_prefer(env->dst_cpu, env->src_cpu);
 }
 
@@ -11645,8 +11644,8 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 				 * env->dst_cpu, so we can't know our idle
 				 * state even if we migrated tasks. Update it.
 				 */
-				idle = idle_cpu(cpu) ? CPU_IDLE : CPU_NOT_IDLE;
-				busy = idle != CPU_IDLE && !sched_idle_cpu(cpu);
+				idle = idle_cpu(cpu);
+				busy = !idle && !sched_idle_cpu(cpu);
 			}
 			sd->last_balance = jiffies;
 			interval = get_sd_balance_interval(sd, busy);
@@ -12343,9 +12342,7 @@ out:
 static __latent_entropy void run_rebalance_domains(struct softirq_action *h)
 {
 	struct rq *this_rq = this_rq();
-	enum cpu_idle_type idle = this_rq->idle_balance ?
-						CPU_IDLE : CPU_NOT_IDLE;
-
+	enum cpu_idle_type idle = this_rq->idle_balance;
 	/*
 	 * If this CPU has a pending nohz_balance_kick, then do the
 	 * balancing on behalf of the other idle CPUs whose ticks are
