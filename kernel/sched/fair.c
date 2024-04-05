70474ed1339579a6fe20d77c3077d33355487658
Peter Zijlstra (Intel) <peterz@infradead.org>
2024-04-05 12:27:56 +0200

    sched/eevdf: Remove min_vruntime_copy
    Since commit e8f331bcc270 ("sched/smp: Use lag to simplify
    cross-runqueue placement") the min_vruntime_copy is no longer used.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -724,8 +724,7 @@ static void update_min_vruntime(struct cfs_rq *cfs_rq)
 	}
 
 	/* ensure we never gain time by being placed backwards. */
-	u64_u32_store(cfs_rq->min_vruntime,
-		      __update_min_vruntime(cfs_rq, vruntime));
+	cfs_rq->min_vruntime = __update_min_vruntime(cfs_rq, vruntime);
 }
 
 static inline bool __entity_less(struct rb_node *a, const struct rb_node *b)
