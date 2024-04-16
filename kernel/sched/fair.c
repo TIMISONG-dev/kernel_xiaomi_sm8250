187d81a094227afb7beffc4f945c788663693583
Juansheng <jaycopter@foxmail.com>
2024-04-16 19:38:29 +0800

    sched/fair: Return early in LB if a wakeup is pending
    by using the new rq->ttwu_pending value.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9780,9 +9780,15 @@ static int should_we_balance(struct lb_env *env)
 	/*
 	 * In the newly idle case, we will allow all the CPUs
 	 * to do the newly idle load balance.
+	 *
+	 * However, we bail out if we already have tasks or a wakeup pending,
+	 * to optimize wakeup latency.
 	 */
-	if (env->idle == CPU_NEWLY_IDLE)
+	if (env->idle == CPU_NEWLY_IDLE) {
+		if (env->dst_rq->nr_running > 0 || env->dst_rq->ttwu_pending)
+			return 0;
 		return 1;
+	}
 
 	/* Try to find first idle CPU */
 	for_each_cpu_and(cpu, group_balance_mask(sg), env->cpus) {
@@ -10930,6 +10936,14 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 		return 0;
 
 	update_misfit_status(NULL, this_rq);
+
+	/*
+	 * There is a task waiting to run. No need to search for one.
+	 * Return 0; the task will be enqueued when switching to idle.
+	 */
+	if (this_rq->ttwu_pending)
+		return 0;
+
 	/*
 	 * We must set idle_stamp _before_ calling idle_balance(), such that we
 	 * measure the duration of idle_balance() as idle time.
@@ -10999,7 +11013,7 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 		 * tasks on this rq or if active migration kicked in.
 		 */
 		if (pulled_task || this_rq->nr_running > 0 ||
-		    !continue_balancing)
+		    this_rq->ttwu_pending)
 			break;
 	}
 	rcu_read_unlock();
