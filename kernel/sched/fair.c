c0b9ac075ef1eea1a5d0d342fbb523f68422b7da
Juansheng <jaycopter@foxmail.com>
2024-04-18 21:09:04 +0800

    sched/fair: eevdf: Add new version of RB_DECLARE_CALLBACKS
    The macro RB_DECLARE_CALLBACKS in this legacy kernel seems incompatible
    with EEVDF's patch. Copy the new version from upstream to make the all
    EEVDF stuff work.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -770,8 +770,8 @@ static inline bool min_vruntime_update(struct sched_entity *se, bool exit)
 	return se->min_vruntime == old_min_vruntime;
 }
 
-RB_DECLARE_CALLBACKS(static, min_vruntime_cb, struct sched_entity,
-		     run_node, min_vruntime, min_vruntime_update);
+RB_DECLARE_CALLBACKS_NEW(static, min_vruntime_cb, struct sched_entity,
+			 run_node, min_vruntime, min_vruntime_update);
 
 /*
  * Enqueue an entity into the rb-tree:
