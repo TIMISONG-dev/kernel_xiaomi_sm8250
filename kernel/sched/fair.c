556848469009ef3b202fc18e78cbe017f151d17a
Juansheng <jaycopter@foxmail.com>
2024-04-21 20:36:24 +0800

    sched/fair: Remove task util_est trace on enqueue

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4207,8 +4207,7 @@ static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 	enqueued += _task_util_est(p);
 	WRITE_ONCE(cfs_rq->avg.util_est, enqueued);
 
-	/* Update plots for Task and CPU estimated utilization */
-	trace_sched_util_est_task(p, &p->se.avg);
+	/* Update plots for CPU's estimated utilization */
 	trace_sched_util_est_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 }
 
