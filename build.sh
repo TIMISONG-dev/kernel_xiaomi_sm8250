5f13782a70e2bbfe62d466d5e4c25a935661c3ac
TIMISONG <dima.pilaer@gmail.com>
2024-05-08 10:01:32 +0300

    bash: fix mistake in posting message

--- build.sh
+++ build.sh
@@ -136,7 +136,7 @@ elapsed_time=$((end_time - start_time))
 # Проверка успешности сборки
 if [ -s "$checkdtb" ]; then
     echo "\e[32mОбщее время выполнения: $elapsed_time секунд\e[0m"
-    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id="@magictimec" -d text="Компиляция завершилась успешно! Время выполенения: $elapsed_time секунд"
+    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id="@magictimec" -d text="Компиляция завершилась успешно! Время выполнения: $elapsed_time секунд"
     curl -F document=@"./MagicTime-$MODEL-$MAGIC_BUILD_DATE.zip" -F caption="$HASH" "https://api.telegram.org/bot$TOKEN/sendDocument?chat_id=@magictimec"
     rm -rf MagicTime-$MODEL-$MAGIC_BUILD_DATE.zip
 else
