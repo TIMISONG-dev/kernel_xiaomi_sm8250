f05b7b68f054c68104d3a9a59674c601ab28b264
Juansheng <jaycopter@foxmail.com>
2024-05-10 10:39:51 +0800

    sched/fair: Rename check_preempt_curr to wakeup_preempt

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1835,7 +1835,7 @@ struct sched_class {
 	void (*yield_task)   (struct rq *rq);
 	bool (*yield_to_task)(struct rq *rq, struct task_struct *p, bool preempt);
 
-	void (*check_preempt_curr)(struct rq *rq, struct task_struct *p, int flags);
+	void (*wakeup_preempt)(struct rq *rq, struct task_struct *p, int flags);
 
 	/*
 	 * Both @prev and @rf are optional and may be NULL, in which case the
@@ -2097,8 +2097,7 @@ static inline void sub_nr_running(struct rq *rq, unsigned count)
 
 extern void activate_task(struct rq *rq, struct task_struct *p, int flags);
 extern void deactivate_task(struct rq *rq, struct task_struct *p, int flags);
-
-extern void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags);
+extern void wakeup_preempt(struct rq *rq, struct task_struct *p, int flags);
 
 extern const_debug unsigned int sysctl_sched_nr_migrate;
 extern const_debug unsigned int sysctl_sched_migration_cost;
