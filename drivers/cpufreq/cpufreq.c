b29a770d30d357efa1a531facad5b8f9b7e4c8c0
Juansheng <jaycopter@foxmail.com>
2024-06-11 22:00:21 +0800

    cpufreq: Don't update per-cpu max_freq_scale
    The per-cpu variable max_freq_scale is not used anymore after
    update_cpu_capacity() in CFS was simplified. So that
    arch_set_max_freq_scale can be removed safely.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2285,8 +2285,6 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	policy->max = new_policy->max;
 	trace_cpu_frequency_limits(policy);
 
-	arch_set_max_freq_scale(policy->cpus, policy->max);
-
 	policy->cached_target_freq = UINT_MAX;
 
 	pr_debug("new min and max freqs are %u - %u kHz\n",
