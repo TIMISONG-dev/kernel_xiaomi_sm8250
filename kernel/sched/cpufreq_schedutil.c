aaac96dd2583fb9a1e496bd14cbd55cee5c9c384
EmanuelCN <emanuelghub@gmail.com>
2024-06-24 15:00:39 +0300

    cpufreq: schedutil: Implement tapered dvfs_headroom
    Ported from: LineageOS/android_kernel_google_gs201@752c5f9
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -234,12 +234,34 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	return l_freq;
 }
 
+static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
+{
+	unsigned int sched_dvfs_headroom[8] = { [0 ... 7] = 1280 };
+	unsigned long capacity = capacity_orig_of(cpu);
+	unsigned long headroom;
+
+	if (util >= capacity)
+		return util;
+
+	/*
+	 * Taper the boosting at the top end as these are expensive and
+	 * we don't need that much of a big headroom as we approach max
+	 * capacity
+	 */
+	headroom = (capacity - util);
+	/* formula: headroom * (1.X - 1) == headroom * 0.X */
+	headroom = headroom *
+		(sched_dvfs_headroom[cpu] - SCHED_CAPACITY_SCALE) >> SCHED_CAPACITY_SHIFT;
+	return util + headroom;
+}
+
+
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
 	unsigned long min,
 	unsigned long max)
 {
 	/* Add dvfs headroom to actual utilization */
-	actual = map_util_perf(actual);
+	actual = apply_dvfs_headroom(actual, cpu);
 	/* Actually we don't need to target the max performance */
 	if (actual < max)
 		max = actual;
