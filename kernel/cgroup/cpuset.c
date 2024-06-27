cdd51d2d1d444523c17a7c59aa32ee20b82858f7
utziacre <utziacre@gmail.com>
2024-06-27 22:49:47 +0800

    cpuset: Don't set cpus allowed for perf critical task

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2496,8 +2496,9 @@ void cpuset_cpus_allowed(struct task_struct *tsk, struct cpumask *pmask)
 void cpuset_cpus_allowed_fallback(struct task_struct *tsk)
 {
 	rcu_read_lock();
-	do_set_cpus_allowed(tsk, is_in_v2_mode() ?
-		task_cs(tsk)->cpus_allowed : cpu_possible_mask);
+	if(!(tsk->flags & PF_PERF_CRITICAL))
+		do_set_cpus_allowed(tsk, is_in_v2_mode() ?
+			task_cs(tsk)->cpus_allowed : cpu_possible_mask);
 	rcu_read_unlock();
 
 	/*
