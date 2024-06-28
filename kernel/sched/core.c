24c7be3853db7684a276425e162016a6c0eae74f
utziacre <utziacre@gmail.com>
2024-06-28 18:05:13 +0800

    uclamp_assist: Tune uclamp values
    Change-Id: Ibdc9bec49b42a27ea61b6dafb74296775fa3e395
    TIMISONG: Remove cpu_shares values

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9847,8 +9847,12 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 	int i;
 
 	static struct uclamp_param tgts[] = {
-		{"top-app",             "1", "max",  1},
-       		{"foreground",          "0", "max",  0},
+		{"top-app",            "10", "max",  1},
+		{"rt",			"0", "max",  1},
+		{"nnapi-hal",		"0", "max",  1},
+       		{"foreground",          "0", "max",  1},
+                {"camera-daemon",      "10", "max",  1},
+                {"system",              "0", "max",  0},
                 {"dex2oat",             "0",  "60",  0},
         	{"background",          "0",  "50",  0},
         	{"system-background",   "0",  "50",  0},
