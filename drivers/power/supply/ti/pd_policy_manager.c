54d6b02713aba7a08f781b620c38c754cf6b6a1b
luffitys <banackidaniel@gmail.com>
2024-07-29 14:39:43 +0200

    drivers: power: supply: ti: Force PD charging if compatible
    Xiaomi only supports PD if the charger is deemed official.
    Bypass this to unlock PD with unofficial chargers.
    Safety measures are not bypassed, as seen in
    pd_get_bms_digest_verified() and usbpd_pm_sm().
    
    Change-Id: Icc99a39ae00ee466f223807621bf9601f44c75ba

--- drivers/power/supply/ti/pd_policy_manager.c
+++ drivers/power/supply/ti/pd_policy_manager.c
@@ -387,10 +387,7 @@ static bool pd_get_bms_digest_verified(struct usbpd_pm *pdpm)
 
 	pr_err("pval.intval: %d\n", pval.intval);
 
-	if (pval.intval == 1)
-		return true;
-	else
-		return false;
+	return true;
 }
 
 /* get bq27z561 chip ok*/
