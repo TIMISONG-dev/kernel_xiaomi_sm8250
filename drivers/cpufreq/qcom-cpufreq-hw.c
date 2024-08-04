52a7c350a3fbaffd7a1007888e0c38b2100eff6b
Mike Tipton <mdtipton@codeaurora.org>
2024-08-04 12:01:56 +0300

    cpufreq: qcom-hw: Fix NULL exception in limits_dcvsh_poll
    The cpufreq framework will set policy->freq_table to NULL after calling
    the exit() callback. If our limits_dcvsh_poll work is scheduled after
    exit(), then it'll cause a NULL exception when qcom_cpufreq_hw_get() is
    called, since that uses policy->freq_table.
    
    To fix this, return early from limits_dcvsh_poll if exit() has already
    been called and schedule the work again after the cpu comes back online.
    
    Change-Id: I2fe099cd05b2ced3dec6c7fe0a8b707e6e15ec8d
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -83,6 +83,7 @@ struct cpufreq_qcom {
 	char dcvsh_irq_name[MAX_FN_SIZE];
 	bool is_irq_enabled;
 	bool is_irq_requested;
+	bool exited;
 };
 
 struct cpufreq_counter {
@@ -167,6 +168,9 @@ static void limits_dcvsh_poll(struct work_struct *work)
 
 	mutex_lock(&c->dcvsh_lock);
 
+	if (c->exited)
+		goto out;
+
 	cpu = cpumask_first(&c->related_cpus);
 
 	freq_limit = limits_mitigation_notify(c, true);
@@ -188,6 +192,7 @@ static void limits_dcvsh_poll(struct work_struct *work)
 		enable_irq(c->dcvsh_irq);
 	}
 
+out:
 	mutex_unlock(&c->dcvsh_lock);
 }
 
@@ -405,6 +410,12 @@ static void qcom_cpufreq_ready(struct cpufreq_policy *policy)
 	static struct thermal_cooling_device *cdev[NR_CPUS];
 	struct device_node *np;
 	unsigned int cpu = policy->cpu;
+
+	struct cpufreq_qcom *c = qcom_freq_domain_map[cpu];
+
+	mutex_lock(&c->dcvsh_lock);
+	c->exited = true;
+	mutex_unlock(&c->dcvsh_lock);
 
 	if (cdev[cpu])
 		return;
