41e38d44e866c81c82d29362300d0c690872878b
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:53 +0200

    sched: Rework pick_next_task()
    The current rule is that:
    
      pick_next_task() := pick_task() + set_next_task(.first = true)
    
    And many classes implement it directly as such. Change things around
    to make pick_next_task() optional while also changing the definition to:
    
      pick_next_task(prev) := pick_task() + put_prev_task() + set_next_task(.first = true)
    
    The reason is that sched_ext would like to have a 'final' call that
    knows the next task. By placing put_prev_task() right next to
    set_next_task() (as it already is for sched_core) this becomes
    trivial.
    
    As a bonus, this is a nice cleanup on its own.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.051225657@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2196,7 +2196,17 @@ struct sched_class {
 
 	void (*wakeup_preempt)(struct rq *rq, struct task_struct *p, int flags);
 
-	struct task_struct *(*pick_next_task)(struct rq *rq);
+	struct task_struct *(*pick_task)(struct rq *rq);
+	/*
+	 * Optional! When implemented pick_next_task() should be equivalent to:
+	 *
+	 *   next = pick_task();
+	 *   if (next) {
+	 *       put_prev_task(prev);
+	 *       set_next_task_first(next);
+	 *   }
+	 */
+	struct task_struct *(*pick_next_task)(struct rq *rq, struct task_struct *prev);
 
 	void (*put_prev_task)(struct rq *rq, struct task_struct *p);
 	void (*set_next_task)(struct rq *rq, struct task_struct *p, bool first);
@@ -2205,7 +2215,6 @@ struct sched_class {
 	int (*balance)(struct rq *rq, struct task_struct *prev, struct rq_flags *rf);
 	int  (*select_task_rq)(struct task_struct *p, int task_cpu, int flags);
 	void (*migrate_task_rq)(struct task_struct *p, int new_cpu);
-	struct task_struct * (*pick_task)(struct rq *rq);
 
 	void (*task_woken)(struct rq *this_rq, struct task_struct *task);
 
@@ -2299,7 +2308,7 @@ static inline bool sched_fair_runnable(struct rq *rq)
 }
 
 extern struct task_struct *pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf);
-extern struct task_struct *pick_next_task_idle(struct rq *rq);
+extern struct task_struct *pick_task_idle(struct rq *rq);
 
 #define SCA_CHECK		0x01
 #define SCA_MIGRATE_DISABLE	0x02
