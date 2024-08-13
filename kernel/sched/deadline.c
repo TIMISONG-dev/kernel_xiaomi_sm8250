5acaa409fb86a0979610daddfbf70fce511a2e16
Peter Zijlstra <peterz@infradead.org>
2024-08-14 00:25:56 +0200

    sched: Add put_prev_task(.next)
    In order to tell the previous sched_class what the next task is, add
    put_prev_task(.next).
    
    Notable SCX will use this to:
    
     1) determine the next task will leave the SCX sched class and push
        the current task to another CPU if possible.
     2) statistics on how often and which other classes preempt it
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20240813224016.367421076@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1887,7 +1887,7 @@ static struct task_struct *pick_task_dl(struct rq *rq)
 	return p;
 }
 
-static void put_prev_task_dl(struct rq *rq, struct task_struct *p)
+static void put_prev_task_dl(struct rq *rq, struct task_struct *p, struct task_struct *next)
 {
 	update_curr_dl(rq);
 
--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -417,7 +417,7 @@ static void check_preempt_curr_idle(struct rq *rq, struct task_struct *p, int fl
 	resched_curr(rq);
 }
 
-static void put_prev_task_idle(struct rq *rq, struct task_struct *prev)
+static void put_prev_task_idle(struct rq *rq, struct task_struct *prev, struct task_struct *next)
 {
 	update_rq_avg_idle(rq);
 }
