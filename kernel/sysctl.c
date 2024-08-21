f46d401bc5fdc0309bd9a2535f2ae63c575bed97
TIMISONG-dev <dima.pilaer@gmail.com>
2024-08-21 21:30:48 +0300

    kernel: Remove unused code

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -639,15 +639,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler   = proc_dointvec,
 	},
 	{
-		.procname	= "sched_min_granularity_ns",
-		.data		= &sysctl_sched_min_granularity,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_proc_update_handler,
-		.extra1		= &min_sched_granularity_ns,
-		.extra2		= &max_sched_granularity_ns,
-	},
-	{
 		.procname	= "sched_sync_hint_enable",
 		.data		= &sysctl_sched_sync_hint_enable,
 		.maxlen		= sizeof(unsigned int),
