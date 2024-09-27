92db3ab278bd73005f22757063de84b7d965d4b9
Tejun Heo <tj@kernel.org>
2024-09-27 13:46:12 -1000

    sched/core: Add ENQUEUE_RQ_SELECTED to indicate whether ->select_task_rq() was called
    During ttwu, ->select_task_rq() can be skipped if only one CPU is allowed or
    migration is disabled. sched_ext schedulers may perform operations such as
    direct dispatch from ->select_task_rq() path and it is useful for them to
    know whether ->select_task_rq() was skipped in the ->enqueue_task() path.
    
    Currently, sched_ext schedulers are using ENQUEUE_WAKEUP for this purpose
    and end up assuming incorrectly that ->select_task_rq() was called for tasks
    that are bound to a single CPU or migration disabled.
    
    Make select_task_rq() indicate whether ->select_task_rq() was called by
    setting WF_RQ_SELECTED in *wake_flags and make ttwu_do_activate() map that
    to ENQUEUE_RQ_SELECTED for ->enqueue_task().
    
    This will be used by sched_ext to fix ->select_task_rq() skip detection.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: David Vernet <void@manifault.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2103,6 +2103,7 @@ static inline int task_on_rq_migrating(struct task_struct *p)
 
 #define WF_SYNC     0x10 /* Waker goes to sleep after wakeup */
 #define WF_MIGRATED 0x20 /* Internal use, task got migrated */
+#define WF_RQ_SELECTED		0x40 /* ->select_task_rq() was called */
 
 #ifdef CONFIG_SMP
 static_assert(WF_EXEC == SD_BALANCE_EXEC);
@@ -2141,6 +2142,7 @@ extern const u32		sched_prio_to_wmult[40];
  * ENQUEUE_HEAD      - place at front of runqueue (tail if not specified)
  * ENQUEUE_REPLENISH - CBS (replenish runtime and postpone deadline)
  * ENQUEUE_MIGRATED  - the task was migrated during wakeup
+ * ENQUEUE_RQ_SELECTED - ->select_task_rq() was called
  *
  */
 
@@ -2166,6 +2168,7 @@ extern const u32		sched_prio_to_wmult[40];
 
 #define ENQUEUE_INITIAL		0x100
 #define ENQUEUE_DELAYED		0x200
+#define ENQUEUE_RQ_SELECTED	0x400
 
 #define ENQUEUE_WAKEUP_SYNC	0x80
 
