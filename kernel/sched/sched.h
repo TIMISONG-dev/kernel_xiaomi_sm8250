ed4aac8e0ab53d2e512e9d1eb212d6e62ad2f719
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-10-19 18:10:25 +0600

    sched/core: Make dequeue_task() public
    dequeue_task() is used by fair.c in switched_from_fair() since [1]. Make it
    public so that fair.c can utilize it.
    
    [1] 9eb83ac9d040 ("BACKPORT: sched/fair: Prepare exit/cleanup paths for delayed_dequeue")
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2699,3 +2699,4 @@ extern void sched_get_nr_running_avg(struct sched_avg_stats *stats);
 
 extern u64 avg_vruntime(struct cfs_rq *cfs_rq);
 extern int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se);
+extern bool dequeue_task(struct rq *rq, struct task_struct *p, int flags);
