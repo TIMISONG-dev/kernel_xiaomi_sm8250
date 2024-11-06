f63c2b029361fba36be98775b248de16fff684c9
Felix Moessbauer <felix.moessbauer@siemens.com>
2024-11-06 22:08:03 +0200

    hrtimer: Use and report correct timerslack values for realtime tasks
    The timerslack_ns setting is used to specify how much the hardware
    timers should be delayed, to potentially dispatch multiple timers in a
    single interrupt. This is a performance optimization. Timers of
    realtime tasks (having a realtime scheduling policy) should not be
    delayed.
    
    This logic was inconsitently applied to the hrtimers, leading to delays
    of realtime tasks which used timed waits for events (e.g. condition
    variables). Due to the downstream override of the slack for rt tasks,
    the procfs reported incorrect (non-zero) timerslack_ns values.
    
    This is changed by setting the timer_slack_ns task attribute to 0 for
    all tasks with a rt policy. By that, downstream users do not need to
    specially handle rt tasks (w.r.t. the slack), and the procfs entry
    shows the correct value of "0". Setting non-zero slack values (either
    via procfs or PR_SET_TIMERSLACK) on tasks with a rt policy is ignored,
    as stated in "man 2 PR_SET_TIMERSLACK":
    
      Timer slack is not applied to threads that are scheduled under a
      real-time scheduling policy (see sched_setscheduler(2)).
    
    The special handling of timerslack on rt tasks in downstream users
    is removed as well.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20240814121032.368444-2-felix.moessbauer@siemens.com

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1818,14 +1818,9 @@ long hrtimer_nanosleep(const struct timespec64 *rqtp,
 	struct restart_block *restart;
 	struct hrtimer_sleeper t;
 	int ret = 0;
-	u64 slack;
-
-	slack = current->timer_slack_ns;
-	if (dl_task(current) || rt_task(current))
-		slack = 0;
 
 	hrtimer_init_on_stack(&t.timer, clockid, mode);
-	hrtimer_set_expires_range_ns(&t.timer, timespec64_to_ktime(*rqtp), slack);
+	hrtimer_set_expires_range_ns(&t.timer, timespec64_to_ktime(*rqtp), current->timer_slack_ns);
 	ret = do_nanosleep(&t, mode);
 	if (ret != -ERESTART_RESTARTBLOCK)
 		goto out;
