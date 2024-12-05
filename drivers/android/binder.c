d1349fb347ea84dcaf254abc2c62d686e46accde
Sean Du <do4suki@gmail.com>
2024-12-05 10:53:49 +0800

    binder: Backport to k4.19
    This reverts the following commits on android13-5.10-lts:
    
    Revert "BACKPORT: FROMGIT: binder: frozen notification binder_features flag"
    
    This reverts commit 41f9c3b9eff3f8df44bbe0fd379970d11081a494.
    
    Revert "UPSTREAM: binderfs: add support for feature files"
    
    This reverts commit 21b60b0671e3e35cd243737ffab5e617bda12cbd.
    
    Revert "binderfs: make symbol 'binderfs_fs_parameters' static"
    
    This reverts commit 89320020d967e8f7affbc4488b85860b3a64c4c4.
    
    Revert "binderfs: remove redundant assignment to pointer ctx"
    
    This reverts commit 9e306ba3a9299fc0348d2345e4cfdb39b77a8a27.
    
    Revert "binderfs: port to new mount api"
    
    This reverts commit 095cf502b31e12317ca309ea49ec69377ea38ea1.
    
    Revert "ANDROID: vendor_hooks: Modify the function name"
    
    This reverts commit 4322bb07e85d6dbaede321f236b43650769d993e.
    
    Revert "ANDROID: Add hook to show vendor info for transactions"
    
    This reverts commit eabe9707f264c859d5b0b5b1740a97f8eef97d67.
    
    Revert "ANDROID: vendor_hooks: Add hooks for binder proc transaction"
    
    This reverts commit 35014985c266face161f6d107f8d8338e02a0b2d.
    
    Revert "ANDROID: vendor_hooks: Add hook for binder"
    
    This reverts commit 2337b9185a7e8ebf9748c9fa8eacc0e2b995797b.
    
    Revert "ANDROID: Add vendor hook to binder."
    
    This reverts commit 301e89472f3e00a2f02f1967d77efd051ada37f5.
    
    Revert "ANDROID: vendor_hooks: Add hooks for improving binder trans"
    
    This reverts commit 1e13e5d9b9ff6ae1b974eaeb38211d4571422dca.
    
    Revert "ANDROID: binder: Export binder_transaction_received trace point"
    
    This reverts commit 85b85c383634690b1fde245b182889b9b5ff0a09.
    
    Revert "ANDROID: binder: consolidate wakeup vendor hooks into one"
    
    This reverts commit 90bd50a4d7c7f31edaa37dbe28a30a2dd76ae997.
    
    Revert "ANDROID: binder: Add vendor hooks"
    
    This reverts commit 37d7ed1b228526cac0b61c7e459129cc96fcac71.
    
    Revert "ANDROID: binder: Add vendor hook to the binder"
    
    This reverts commit 3fc5e942554eb435a2afd5113778eefd2a97f19b.
    
    Revert "ANDROID: Add oem data array for async binder transaction"
    
    This reverts commit 3ad47a04c95bf75a174f6c86e4edd5920d210050.
    
    Revert "mmap locking API: convert mmap_sem API comments"
    
    This reverts commit 3e4e28c5a8f01ee4174d639e36ed155ade489a6f.
    
    Revert "FROMLIST: binder: fix UAF of alloc->vma in race with munmap()"
    
    This reverts commit b684150a44187376ae2ea8369c400140ffc3ea67.
    
    Revert "mmap locking API: use coccinelle to convert mmap_sem rwsem call sites"
    
    This reverts commit d8ed45c5dcd455fc5848d47f86883a1b872ac0d0.
    
    Revert "task_work: cleanup notification modes"
    
    This reverts commit 91989c707884ecc7cd537281ab1a4b8fb7219da3.
    
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Ia3f3c611add39c9616e58dcb4642b268f5f1925e

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -66,7 +66,6 @@
 #include <linux/syscalls.h>
 #include <linux/task_work.h>
 #include <linux/sizes.h>
-#include <linux/android_vendor.h>
 
 #include <uapi/linux/sched/types.h>
 #include <uapi/linux/android/binder.h>
@@ -75,7 +74,6 @@
 
 #include "binder_internal.h"
 #include "binder_trace.h"
-#include <trace/hooks/binder.h>
 
 static HLIST_HEAD(binder_deferred_list);
 static DEFINE_MUTEX(binder_deferred_lock);
@@ -574,7 +572,6 @@ static void binder_wakeup_poll_threads_ilocked(struct binder_proc *proc,
 				(current->prio < DEFAULT_PRIO))
 				thread->task->low_latency = true;
 #endif
-			trace_android_vh_binder_wakeup_ilocked(thread->task, sync, proc);
 			if (sync)
 				wake_up_interruptible_sync(&thread->wait);
 			else
@@ -640,7 +637,6 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 			(current->prio < DEFAULT_PRIO))
 			thread->task->low_latency = true;
 #endif
-		trace_android_vh_binder_wakeup_ilocked(thread->task, sync, proc);
 		if (sync)
 			wake_up_interruptible_sync(&thread->wait);
 		else
@@ -810,16 +806,11 @@ static void binder_transaction_priority(struct binder_thread *thread,
 		.sched_policy = node->sched_policy,
 		.prio = node->min_priority,
 	};
-	bool skip = false;
 
 	if (t->set_priority_called)
 		return;
 
 	t->set_priority_called = true;
-
-	trace_android_vh_binder_priority_skip(task, &skip);
-	if (skip)
-		return;
 
 	if (!node->inherit_rt && is_rt_policy(desired.sched_policy)) {
 		desired.prio = NICE_TO_PRIO(0);
@@ -859,7 +850,6 @@ static void binder_transaction_priority(struct binder_thread *thread,
 	spin_unlock(&thread->prio_lock);
 
 	binder_set_priority(thread, &desired);
-	trace_android_vh_binder_set_priority(t, task);
 }
 
 static struct binder_node *binder_get_node_ilocked(struct binder_proc *proc,
@@ -1291,7 +1281,6 @@ static struct binder_ref *binder_get_ref_for_node_olocked(
 		     "%d new ref %d desc %d for node %d\n",
 		      proc->pid, new_ref->data.debug_id, new_ref->data.desc,
 		      node->debug_id);
-	trace_android_vh_binder_new_ref(proc->tsk, new_ref->data.desc, new_ref->node->debug_id);
 	binder_node_unlock(node);
 	return new_ref;
 }
@@ -1463,7 +1452,6 @@ err_no_ref:
  */
 static void binder_free_ref(struct binder_ref *ref)
 {
-	trace_android_vh_binder_del_ref(ref->proc ? ref->proc->tsk : 0, ref->data.desc);
 	if (ref->node)
 		binder_free_node(ref->node);
 	kfree(ref->death);
@@ -2091,7 +2079,7 @@ static void binder_deferred_fd_close(int fd)
 	close_fd_get_file(fd, &twcb->file);
 	if (twcb->file) {
 		filp_close(twcb->file, current->files);
-		task_work_add(current, &twcb->twork, TWA_RESUME);
+		task_work_add(current, &twcb->twork, true);
 	} else {
 		kfree(twcb);
 	}
@@ -2945,9 +2933,6 @@ static int binder_proc_transaction(struct binder_transaction *t,
 	if (!thread && !pending_async)
 		thread = binder_select_thread_ilocked(proc);
 
-	trace_android_vh_binder_proc_transaction(current, proc->tsk,
-		thread ? thread->task : 0, node->debug_id, t->code, pending_async);
-
 	if (thread) {
 		binder_transaction_priority(thread, t, node);
 		binder_enqueue_thread_work_ilocked(thread, &t->work);
@@ -3137,7 +3122,6 @@ static void binder_transaction(struct binder_proc *proc,
 		target_proc = target_thread->proc;
 		target_proc->tmp_ref++;
 		binder_inner_proc_unlock(target_thread->proc);
-		trace_android_vh_binder_reply(target_proc, proc, thread, tr);
 	} else {
 		if (tr->target.handle) {
 			struct binder_ref *ref;
@@ -3196,7 +3180,6 @@ static void binder_transaction(struct binder_proc *proc,
 			return_error_line = __LINE__;
 			goto err_invalid_target_handle;
 		}
-		trace_android_vh_binder_trans(target_proc, proc, thread, tr);
 		if (security_binder_transaction(proc->cred,
 						target_proc->cred) < 0) {
 			return_error = BR_FAILED_REPLY;
@@ -3279,7 +3262,6 @@ static void binder_transaction(struct binder_proc *proc,
 	INIT_LIST_HEAD(&t->fd_fixups);
 	binder_stats_created(BINDER_STAT_TRANSACTION);
 	spin_lock_init(&t->lock);
-	trace_android_vh_binder_transaction_init(t);
 
 	tcomplete = kzalloc(sizeof(*tcomplete), GFP_KERNEL);
 	if (tcomplete == NULL) {
@@ -3746,7 +3728,6 @@ static void binder_transaction(struct binder_proc *proc,
 			spin_unlock(&thread->prio_lock);
 		}
 		wake_up_interruptible_sync(&target_thread->wait);
-		trace_android_vh_binder_restore_priority(in_reply_to, current);
 		binder_restore_priority(thread, &in_reply_to->saved_priority);
 		binder_free_transaction(in_reply_to);
 	} else if (!(t->flags & TF_ONE_WAY)) {
@@ -3865,7 +3846,6 @@ err_invalid_target_handle:
 
 	BUG_ON(thread->return_error.cmd != BR_OK);
 	if (in_reply_to) {
-		trace_android_vh_binder_restore_priority(in_reply_to, current);
 		binder_restore_priority(thread, &in_reply_to->saved_priority);
 		thread->return_error.cmd = BR_TRANSACTION_COMPLETE;
 		binder_enqueue_thread_work(thread, &thread->return_error.work);
@@ -4610,7 +4590,6 @@ static int binder_wait_for_work(struct binder_thread *thread,
 		if (do_proc_work)
 			list_add(&thread->waiting_thread_node,
 				 &proc->waiting_threads);
-		trace_android_vh_binder_wait_for_work(do_proc_work, thread, proc);
 		binder_inner_proc_unlock(proc);
 		schedule();
 		binder_inner_proc_lock(proc);
@@ -4728,7 +4707,6 @@ retry:
 			wait_event_interruptible(binder_user_error_wait,
 						 binder_stop_on_user_error < 2);
 		}
-		trace_android_vh_binder_restore_priority(NULL, current);
 		binder_restore_priority(thread, &proc->default_priority);
 	}
 
@@ -5016,7 +4994,6 @@ retry:
 			trd->sender_pid =
 				task_tgid_nr_ns(sender,
 						task_active_pid_ns(current));
-			trace_android_vh_sync_txn_recvd(thread->task, t_from->task);
 		} else {
 			trd->sender_pid = 0;
 		}
@@ -6084,7 +6061,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	}
 	hlist_add_head(&proc->proc_node, &binder_procs);
 	mutex_unlock(&binder_procs_lock);
-	trace_android_vh_binder_preset(&binder_procs, &binder_procs_lock);
+
 	if (binder_debugfs_dir_entry_proc && !existing_pid) {
 		char strbuf[11];
 
@@ -6379,7 +6356,6 @@ static void print_binder_transaction_ilocked(struct seq_file *m,
 	struct binder_buffer *buffer = t->buffer;
 
 	spin_lock(&t->lock);
-	trace_android_vh_binder_print_transaction_info(m, proc, prefix, t);
 	to_proc = t->to_proc;
 	seq_printf(m,
 		   "%s %d: %pK from %d:%d to %d:%d code %x flags %x pri %d:%d r%d",
@@ -7064,6 +7040,5 @@ device_initcall(binder_init);
 
 #define CREATE_TRACE_POINTS
 #include "binder_trace.h"
-EXPORT_TRACEPOINT_SYMBOL_GPL(binder_transaction_received);
 
 MODULE_LICENSE("GPL v2");
