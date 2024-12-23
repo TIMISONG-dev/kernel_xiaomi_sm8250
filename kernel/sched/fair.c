69cf00ad6e9791324e72f720e2ab6c8e0a41fc8b
K Prateek Nayak <kprateek.nayak@amd.com>
2024-12-23 04:34:06 +0000

    sched/fair: Do not compute overloaded status unnecessarily during lb
    Only set sg_overloaded when computing sg_lb_stats() at the highest sched
    domain since rd->overloaded status is updated only when load balancing
    at the highest domain. While at it, move setting of sg_overloaded below
    idle_cpu() check since an idle CPU can never be overloaded.
    
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Link: https://lore.kernel.org/r/20241223043407.1611-8-kprateek.nayak@amd.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9118,6 +9118,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 				      int *sg_status)
 {
 	int i, nr_running, local_group;
+	bool balancing_at_rd = !env->sd->parent;
 
 	memset(sgs, 0, sizeof(*sgs));
 
@@ -9135,9 +9136,6 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		nr_running = rq->nr_running;
 		sgs->sum_nr_running += nr_running;
 
-		if (nr_running > 1)
-			*sg_status |= SG_OVERLOAD;
-
 		if (cpu_overutilized(i))
 			*sg_status |= SG_OVERUTILIZED;
 
@@ -9153,6 +9151,10 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			/* Idle cpu can't have misfit task */
 			continue;
 		}
+
+		/* Overload indicator is only updated at root domain */
+		if (balancing_at_rd && nr_running > 1)
+			*sg_status |= SG_OVERLOAD;
 
 		if (local_group)
 			continue;
