030bc56ca17ebb0f39f8122884e8c19c7eab4a85
Sultan Alsawaf <sultan@kerneltoast.com>
2025-02-05 18:57:32 -0800

    arch_topology: Introduce minimum frequency scale
    The scheduler is unaware of the applied min_freq limit to a CPU, which is
    useful information when predicting the frequency a CPU will run at for
    energy efficiency purposes.
    
    Export this information via arch_scale_min_freq_capacity() and wire it up
    for arm64.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -47,6 +47,8 @@ int pcibus_to_node(struct pci_bus *bus);
 
 /* Replace task scheduler's default max-frequency-invariant accounting */
 #define arch_scale_max_freq_capacity topology_get_max_freq_scale
+#define arch_set_min_freq_scale topology_set_min_freq_scale
+#define arch_scale_min_freq_capacity topology_get_min_freq_scale
 
 /* Replace task scheduler's default cpu-invariant accounting */
 #define arch_scale_cpu_capacity topology_get_cpu_scale
