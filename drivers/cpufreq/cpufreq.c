d1d1e714fee413ef8c9d5de8547d621a75139cde
Sultan Alsawaf <sultan@kerneltoast.com>
2025-02-05 18:57:32 -0800

    arch_topology: Introduce minimum frequency scale
    The scheduler is unaware of the applied min_freq limit to a CPU, which is
    useful information when predicting the frequency a CPU will run at for
    energy efficiency purposes.
    
    Export this information via arch_scale_min_freq_capacity() and wire it up
    for arm64.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2333,6 +2333,9 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	policy->max = new_policy->max;
 	trace_cpu_frequency_limits(policy);
 
+	arch_set_min_freq_scale(policy->related_cpus, policy->min,
+				policy->cpuinfo.max_freq);
+
 	policy->cached_target_freq = UINT_MAX;
 
 	pr_debug("new min and max freqs are %u - %u kHz\n",
