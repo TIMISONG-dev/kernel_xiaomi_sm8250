a74f2d10b3b1c40920cf337aa30f4154f9ab599b
Sultan Alsawaf <sultan@kerneltoast.com>
2025-02-05 18:57:32 -0800

    arch_topology: Introduce minimum frequency scale
    The scheduler is unaware of the applied min_freq limit to a CPU, which is
    useful information when predicting the frequency a CPU will run at for
    energy efficiency purposes.
    
    Export this information via arch_scale_min_freq_capacity() and wire it up
    for arm64.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2148,6 +2148,14 @@ unsigned long arch_scale_max_freq_capacity(struct sched_domain *sd, int cpu)
 }
 #endif
 
+#ifndef arch_scale_min_freq_capacity
+static __always_inline
+unsigned long arch_scale_min_freq_capacity(int cpu)
+{
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_SMP
 static inline unsigned long capacity_of(int cpu)
 {
