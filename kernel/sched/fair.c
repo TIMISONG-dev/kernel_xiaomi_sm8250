9e5df0f7e8556df5c309bb9f2c957523dd4fb0f2
Tianchen Ding <dtcccc@linux.alibaba.com>
2025-02-11 14:36:59 +0800

    sched/eevdf: Force propagating min_slice of cfs_rq when {en,de}queue tasks
    When a task is enqueued and its parent cgroup se is already on_rq, this
    parent cgroup se will not be enqueued again, and hence the root->min_slice
    leaves unchanged. The same issue happens when a task is dequeued and its
    parent cgroup se has other runnable entities, and the parent cgroup se
    will not be dequeued.
    
    Force propagating min_slice when se doesn't need to be enqueued or
    dequeued. Ensure the se hierarchy always get the latest min_slice.
    
    Fixes: aef6987d8954 ("sched/eevdf: Propagate min_slice up the cgroup hierarchy")
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20250211063659.7180-1-dtcccc@linux.alibaba.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6051,6 +6051,8 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_cfs_group(se);
 
 		se->slice = slice;
+		if (se != cfs_rq->curr)
+			min_vruntime_cb_propagate(&se->run_node, NULL);
 		slice = cfs_rq_min_slice(cfs_rq);
 
 		cfs_rq->h_nr_running++;
@@ -6186,6 +6188,8 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags)
 		update_cfs_group(se);
 
 		se->slice = slice;
+		if (se != cfs_rq->curr)
+			min_vruntime_cb_propagate(&se->run_node, NULL);
 		slice = cfs_rq_min_slice(cfs_rq);
 
 		cfs_rq->h_nr_running -= h_nr_running;
