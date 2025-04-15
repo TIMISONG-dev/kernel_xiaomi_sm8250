6e19392f71e230bdbff66159c0e25f4f9febc7b1
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-04-15 11:58:08 +0200

    cpufreq/sched: Fix the usage of CPUFREQ_NEED_UPDATE_LIMITS
    Commit 6a640815b9efe610be491b14de8202b074d10179 ("cpufreq: schedutil: Fix superfluous updates caused
    by need_freq_update") modified sugov_should_update_freq() to set the
    need_freq_update flag only for drivers with CPUFREQ_NEED_UPDATE_LIMITS
    set, but that flag generally needs to be set when the policy limits
    change because the driver callback may need to be invoked for the new
    limits to take effect.
    
    However, if the return value of cpufreq_driver_resolve_freq() after
    applying the new limits is still equal to the previously selected
    frequency, the driver callback needs to be invoked only in the case
    when CPUFREQ_NEED_UPDATE_LIMITS is set (which means that the driver
    specifically wants its callback to be invoked every time the policy
    limits change).
    
    Update the code accordingly to avoid missing policy limits changes for
    drivers without CPUFREQ_NEED_UPDATE_LIMITS.
    
    Fixes: 6a640815b9efe610be491b14de8202b074d10179 ("cpufreq: schedutil: Fix superfluous updates caused by need_freq_update")
    Closes: https://lore.kernel.org/lkml/Z_Tlc6Qs-tYpxWYb@linaro.org/
    Reported-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Link: https://patch.msgid.link/3010358.e9J7NaK4W3@rjwysocki.net
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -99,7 +99,7 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 
 	if (unlikely(sg_policy->limits_changed)) {
 		sg_policy->limits_changed = false;
-		sg_policy->need_freq_update = cpufreq_driver_test_flags(CPUFREQ_NEED_UPDATE_LIMITS);
+		sg_policy->need_freq_update = true;
 		return true;
 	}
 
@@ -129,12 +129,24 @@ static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 	 * systems. A check for arch_scale_freq_invariant() is omitted here
 	 * because unconditionally rechecking the rate limit is cheaper.
 	 */
-	if (sg_policy->need_freq_update)
+	if (sg_policy->need_freq_update) {
 		sg_policy->need_freq_update = false;
-	else if (next_freq == sg_policy->next_freq ||
+		/*
+		 * The policy limits have changed, but if the return value of
+		 * cpufreq_driver_resolve_freq() after applying the new limits
+		 * is still equal to the previously selected frequency, the
+		 * driver callback need not be invoked unless the driver
+		 * specifically wants that to happen on every update of the
+		 * policy limits.
+		 */
+		 if (sg_policy->next_freq == next_freq &&
+		    !cpufreq_driver_test_flags(CPUFREQ_NEED_UPDATE_LIMITS))
+			return false;
+	} else if (next_freq == sg_policy->next_freq ||
 		 (next_freq < sg_policy->next_freq &&
-		  sugov_should_rate_limit(sg_policy, time)))
+		  sugov_should_rate_limit(sg_policy, time))) {
 		return false;
+	}
 
 	sg_policy->next_freq = next_freq;
 	sg_policy->last_freq_update_time = time;
