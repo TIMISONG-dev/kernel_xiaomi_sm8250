432198cc0f4c671a0fcfb1d889ad2c6131387042
Rocky7842 <eric.rocky7842@gmail.com>
2025-05-29 02:19:44 +0800

    techpack: camera-xiaomi-psyche: Implement OIS post_init functions
    * Kanged from xiaomi sm8350. Required by EIS.
    
    Change-Id: I899afe61f34314e8d6215da999cd57a0e169be9e

--- techpack/camera-xiaomi-psyche/include/uapi/media/cam_sensor.h
+++ techpack/camera-xiaomi-psyche/include/uapi/media/cam_sensor.h
@@ -137,6 +137,7 @@ struct cam_ois_opcode {
  * @ois_fw_flag           :    indicates if fw is present or not
  * @is_ois_calib          :    indicates the calibration data is available
  * @is_ois_pre_init       :    indicates the pre initialize data is available
+ * @is_ois_post_init      :    indicates the post initialize data is available
  * @ois_name              :    OIS name
  * @opcode                :    opcode
  */
@@ -147,6 +148,7 @@ struct cam_cmd_ois_info {
 	uint8_t               ois_fw_flag;
 	uint8_t               is_ois_calib;
 	uint8_t               is_ois_pre_init; //xiaomi add
+	uint8_t               is_ois_post_init; //xiaomi add
 	char                  ois_name[MAX_OIS_NAME_SIZE];
 	struct cam_ois_opcode opcode;
 } __attribute__((packed));
