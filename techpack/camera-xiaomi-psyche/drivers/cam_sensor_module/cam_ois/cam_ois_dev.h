f335e6d0a8dd640f771b173e4637d282e388928f
Rocky7842 <eric.rocky7842@gmail.com>
2025-05-29 02:19:44 +0800

    techpack: camera-xiaomi-psyche: Implement OIS post_init functions
    * Kanged from xiaomi sm8350. Required by EIS.
    
    Change-Id: I899afe61f34314e8d6215da999cd57a0e169be9e

--- techpack/camera-xiaomi-psyche/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
+++ techpack/camera-xiaomi-psyche/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
@@ -112,6 +112,8 @@ struct ois_data_eis_t {
  * @device_name     :   Device name
  * @i2c_pre_init_data:  ois i2c pre init settings
  * @is_ois_pre_init :   flag for pre init settings
+ * @i2c_post_init_data: ois i2c post init settings
+ * @is_ois_post_init :  flag for post init settings
  *
  */
 struct cam_ois_ctrl_t {
@@ -134,7 +136,9 @@ struct cam_ois_ctrl_t {
 	uint8_t is_ois_calib;
 	struct cam_ois_opcode opcode;
 	struct i2c_settings_array i2c_pre_init_data; //xiaomi add
+	struct i2c_settings_array i2c_post_init_data; //xiaomi add
 	uint8_t is_ois_pre_init; //xiaomi add
+	uint8_t is_ois_post_init; //xiaomi add
 #ifdef ENABLE_OIS_EIS
 	struct ois_data_eis_t ois_data;
 #endif
