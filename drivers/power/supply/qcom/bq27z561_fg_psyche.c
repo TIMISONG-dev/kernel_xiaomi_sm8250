5e9777e6f0e3fdd02c1b0468958817d59784641c
Flicker372 <flicker372@outlook.com>
2025-06-01 17:32:37 +0800

    drivers: power: supply: psyche: Increase battery undervoltage threshold
    * Fixed the issue that low battery automatic shutdown could not be triggered
    * The battery undervoltage threshold set by Xiaomi is too low, and there is no notification system shutdown implementation
    
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- drivers/power/supply/qcom/bq27z561_fg_psyche.c
+++ drivers/power/supply/qcom/bq27z561_fg_psyche.c
@@ -204,6 +204,7 @@ struct bq_fg_chip {
 
 	bool batt_fc;
 	bool batt_fd;	/* full depleted */
+	bool shutdown_soc;	/* shutdown */
 
 	bool batt_dsg;
 	bool batt_rca;	/* remaining capacity alarm */
@@ -1381,13 +1382,15 @@ static int fg_get_batt_status(struct bq_fg_chip *bq)
 static int fg_get_batt_capacity_level(struct bq_fg_chip *bq)
 {
 
-	if (bq->batt_fc)
+	if (bq->batt_fc) {
 		return POWER_SUPPLY_CAPACITY_LEVEL_FULL;
-	else if (bq->batt_rca && bq->batt_soc <= 15)
+	} else if (bq->shutdown_soc) {
+		return POWER_SUPPLY_CAPACITY_LEVEL_CRITICAL;
+	} else if (bq->batt_rca && bq->batt_soc <= 15) {
 		return POWER_SUPPLY_CAPACITY_LEVEL_LOW;
-	else if (bq->batt_fd && bq->batt_soc <= 15)
+	} else if (bq->batt_fd && bq->batt_soc <= 15) {
 		return POWER_SUPPLY_CAPACITY_LEVEL_LOW;
-	else
+	} else
 		return POWER_SUPPLY_CAPACITY_LEVEL_NORMAL;
 
 }
@@ -1525,7 +1528,7 @@ static enum power_supply_property fg_props[] = {
 	POWER_SUPPLY_PROP_SOH,
 };
 
-#define SHUTDOWN_DELAY_VOL	3300
+#define SHUTDOWN_DELAY_VOL	3410
 static int fg_get_property(struct power_supply *psy, enum power_supply_property psp,
 					union power_supply_propval *val)
 {
@@ -1610,8 +1613,11 @@ static int fg_get_property(struct power_supply *psy, enum power_supply_property 
 					val->intval = 1;
 				} else {
 					bq->shutdown_delay = false;
-					if (shutdown_delay_cancel[bq->fg_index])
+					if (shutdown_delay_cancel[bq->fg_index]) {
 						val->intval = 1;
+					} else {
+						bq->shutdown_soc = true;
+					}
 				}
 			} else {
 				bq->shutdown_delay = false;
@@ -2897,6 +2903,7 @@ static int bq_fg_probe(struct i2c_client *client,
 	bq->fake_temp	= -EINVAL;
 	bq->fake_volt	= -EINVAL;
 	bq->fake_chip_ok = -EINVAL;
+	bq->shutdown_soc = false;
 #ifdef CONFIG_DUAL_FUEL_GAUGE_BQ27Z561
 	FG_REPORT_FULL_SOC = FG_REPORT_FULL_SOC_DEVICE;
 #else
