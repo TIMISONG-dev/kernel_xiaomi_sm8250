4918a07bd592562f02aa2f9602ea189856f4e457
TIMISONG-dev <dima.pilaer@gmail.com>
2025-06-02 21:30:17 +0300

    sched/numa: Allow a floating imbalance between NUMA nodes
    refs: Atom-X-Devs/scarlet_xiaomi_sm7325@779daf9
    
    Add new code with CONFIG_NUMA conditions

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1357,16 +1357,14 @@ update_stats_curr_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #ifdef CONFIG_NUMA
 #define NUMA_IMBALANCE_MIN 2
 
-static inline long adjust_numa_imbalance(int imbalance, int dst_running)
+static inline long adjust_numa_imbalance(int imbalance, int dst_running, int dst_weight)
 {
-	unsigned int imbalance_min;
 
 	/*
 	 * Allow a small imbalance based on a simple pair of communicating
-	 * tasks that remain local when the source domain is almost idle.
+	 * tasks that remain local when the destination is lightly loaded.
 	 */
-	imbalance_min = NUMA_IMBALANCE_MIN;
-	if (dst_running <= imbalance_min)
+	if (dst_running < (dst_weight >> 2) && imbalance <= NUMA_IMBALANCE_MIN)
 		return 0;
 
 	return imbalance;
@@ -1905,6 +1903,10 @@ struct task_numa_env {
 static unsigned long cpu_load(struct rq *rq);
 static unsigned long cpu_runnable(struct rq *rq);
 static unsigned long cpu_util(int cpu);
+#ifdef CONFIG_NUMA
+static inline long adjust_numa_imbalance(int imbalance,
+					int dst_running, int dst_weight);
+#endif
 
 static inline enum
 numa_type numa_classify(unsigned int imbalance_pct,
@@ -2284,7 +2286,8 @@ static void task_numa_find_cpu(struct task_numa_env *env,
 		src_running = env->src_stats.nr_running - 1;
 		dst_running = env->dst_stats.nr_running + 1;
 		imbalance = max(0, dst_running - src_running);
-		imbalance = adjust_numa_imbalance(imbalance, dst_running);
+		imbalance = adjust_numa_imbalance(imbalance, dst_running,
+			env->dst_stats.weight);
 
 		/* Use idle CPU if there is no imbalance */
 		if (!imbalance) {
@@ -10087,9 +10090,10 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 
 #ifdef CONFIG_NUMA
 		/* Consider allowing a small imbalance between NUMA groups */
-		if (env->sd->flags & SD_NUMA)
+		if (env->sd->flags & SD_NUMA) {
 			env->imbalance = adjust_numa_imbalance(env->imbalance,
-						busiest->sum_nr_running);
+						busiest->sum_nr_running, busiest->group_weight);
+		}
 #endif
 
 		return;
