1fe814cc952235accabc46220f198d62f09eb693
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-06-17 00:00:51 +0600

    cpufreq: schedutil: Optimize quadratic headroom calculation
    Simplify the quadratic tapering logic for DVFS headroom by eliminating
    unnecessary multiplication and addition operations, and constant factor
    array access, and thus achieving faster computation in the schedutil
    hot-path.
    
    TEST:
    
    capacity = 1024
    
    For util = 90% of capacity = 1024 * 90% = 922,
      Original:
       SCHED_CAPACITY_SCALE = 1024
       SCHED_CAPACITY_SHIFT = 10
       factor[cpu] = 1280 - 1024 = 256
       delta = 1024 - 922 = 102
       headroom = (102 * 102 * 256 >> (10 + 10)) = 2
    
      New:
       delta = 1024 - 922 = 102
       headroom = ((102 * 102) >> 12) = 2
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -234,23 +234,21 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	return l_freq;
 }
 
-static const unsigned long factor[8] = { [0 ... 7] = 1280 - SCHED_CAPACITY_SCALE };
-
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
 	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta;
-	unsigned long headroom;
+	unsigned long delta, headroom;
 
 	if (util >= capacity)
 		return util;
 
 	/*
-	 * Quadratic taper the boosting at the top end as these are expensive and
-	 * we don't need that much of a big headroom as we approach max capacity
+	 * Quadratic taper the boosting at the top end as these are expensive
+	 * and we don't need that much of a big headroom as we approach max
+	 * capacity
 	 */
 	delta = capacity - util;
-	headroom = (delta * delta * factor[cpu]) >> (SCHED_CAPACITY_SHIFT + 10);
+	headroom = ((delta * delta) >> 12);
 
 	return util + headroom;
 }
