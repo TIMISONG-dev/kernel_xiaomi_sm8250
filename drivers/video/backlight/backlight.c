465201f1dbe3f4b5c5cb8200871d27012da4b35d
TIMISONG-dev <dima.pilaer@gmail.com>
2025-06-19 21:45:20 +0300

    drivers/backlight: Remove unnecessary condition

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -215,12 +215,6 @@ static ssize_t brightness_store(struct device *dev,
 		return rc;
 
 	bd->usr_brightness_req = brightness;
-#ifndef CONFIG_BOARD_XIAOMI
-	brightness = (brightness <= bd->thermal_brightness_limit) ?
-				bd->usr_brightness_req :
-				bd->thermal_brightness_limit;
-#endif
-
 	rc = backlight_device_set_brightness(bd, brightness);
 
 	return rc ? rc : count;
