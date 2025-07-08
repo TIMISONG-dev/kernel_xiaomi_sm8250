4a4f1e3032443c54513e4cc55dc58b580963ef5c
Vincent Guittot <vincent.guittot@linaro.org>
2025-07-08 18:56:30 +0200

    sched/fair: Always trigger resched at the end of a protected period
    Always trigger a resched after a protected period even if the entity is
    still eligible. It can happen that an entity remains eligible at the end
    of the protected period but must let an entity with a shorter slice to run
    in order to keep its lag shorter than slice. This is particulalry true
    with run to parity which tries to maximize the lag.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20250708165630.1948751-7-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1112,14 +1112,6 @@ static void update_tg_load_avg(struct cfs_rq *cfs_rq)
 }
 #endif /* CONFIG_SMP */
 
-static inline bool resched_next_slice(struct cfs_rq *cfs_rq, struct sched_entity *curr)
-{
-	if (protect_slice(curr))
-		return false;
-
-	return !entity_eligible(cfs_rq, curr);
-}
-
 /*
  * Update the current task's runtime statistics.
  */
@@ -1163,7 +1155,7 @@ static void update_curr(struct cfs_rq *cfs_rq)
 	if (cfs_rq->nr_running == 1)
 		return;
 
-	if (resched || resched_next_slice(cfs_rq, curr)) {
+	if (resched || !protect_slice(curr)) {
 		resched_curr(rq);
 		clear_buddies(cfs_rq, curr);
 	}
