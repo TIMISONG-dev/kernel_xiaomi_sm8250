f12d281c4ad68cfcb6e0aef743b271db717e20b6
EmanuelCN <emanuelghub@gmail.com>
2025-08-21 12:38:12 +0300

    kernel/sched: Set sched_uclamp_util_min to SCHED_CAPACITY_SCALE
    That's how every oem has it set.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -780,7 +780,7 @@ static void set_load_weight(struct task_struct *p)
 static DEFINE_MUTEX(uclamp_mutex);
 
 /* Max allowed minimum utilization */
-unsigned int sysctl_sched_uclamp_util_min = 128;
+unsigned int sysctl_sched_uclamp_util_min = SCHED_CAPACITY_SCALE;
 
 /* Max allowed maximum utilization */
 unsigned int sysctl_sched_uclamp_util_max = SCHED_CAPACITY_SCALE;
