18a9593fe264051c9e98664a634cde6519e0efc3
EmanuelCN <emanuelghub@gmail.com>
2025-09-05 17:41:01 +0300

    mm/shmem: Convert to XArray

--- mm/shmem.c
+++ mm/shmem.c
@@ -1622,7 +1622,7 @@ static int shmem_swapin_page(struct inode *inode, pgoff_t index,
 	swp_entry_t swap;
 	int error;
 
-	VM_BUG_ON(!*pagep || !radix_tree_exceptional_entry(*pagep));
+	VM_BUG_ON(!*pagep || !xa_is_value(*pagep));
 	swap = radix_to_swp_entry(*pagep);
 	*pagep = NULL;
 
@@ -1756,7 +1756,7 @@ repeat:
 	page = find_lock_entry(mapping, index);
 
 	if (page && vma && userfaultfd_minor(vma)) {
-		if (!radix_tree_exceptional_entry(page)) {
+		if (!xa_is_value(page)) {
 			unlock_page(page);
 			put_page(page);
 		}
@@ -1764,7 +1764,7 @@ repeat:
 		return 0;
 	}
 
-	if (radix_tree_exceptional_entry(page)) {
+	if (xa_is_value(page)) {
 		error = shmem_swapin_page(inode, index, &page,
 					  sgp, gfp, vma, fault_type);
 		if (error == -EEXIST)
