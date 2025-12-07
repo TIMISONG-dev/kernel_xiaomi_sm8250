cf3cea94aa952db21755d8404cf60974c204817e
TIMISONG-dev <dima.pilaer@gmail.com>
2025-12-07 15:07:54 +0300

    Partially revert "sched: deprecate sched_is_energy_aware() and energy_aware()"

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1536,7 +1536,7 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 	 * will have to sort it out.
 	 */
 	may_not_preempt = task_may_not_preempt(curr, cpu);
-	if (static_branch_unlikely(&sched_energy_present) || may_not_preempt ||
+	if (energy_aware() || may_not_preempt ||
 	    (unlikely(rt_task(curr)) &&
 	     (curr->nr_cpus_allowed < 2 ||
 	      curr->prio <= p->prio))) {
