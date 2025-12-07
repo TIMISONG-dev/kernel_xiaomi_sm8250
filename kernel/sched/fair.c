20053d1b4186ee4c2fe9727c9ffdaca62f16889c
TIMISONG-dev <dima.pilaer@gmail.com>
2025-12-07 21:18:21 +0300

    ANDROID: sched/fair: Sync task util early in feec
    refs: Atom-X-Devs/scarlet_xiaomi_sm7325@c883c4b

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7378,6 +7378,8 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 	struct perf_domain *pd;
 	struct energy_env eenv;
 
+	sync_entity_load_avg(&p->se);
+
 	rcu_read_lock();
 	pd = rcu_dereference(rd->pd);
 	if (!pd || READ_ONCE(rd->overutilized))
@@ -7403,7 +7405,6 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 
 	target = prev_cpu;
 
-	sync_entity_load_avg(&p->se);
 	if (!task_util_est(p) && p_util_min == 0)
 		goto unlock;
 
