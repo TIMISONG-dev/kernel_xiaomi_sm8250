7bf14052f89c7bfdd5b715c236a7200e69f774fe
TIMISONG-dev <dima.pilaer@gmail.com>
2025-12-18 23:34:58 +0300

    kernel: sucompat: Remove inode_security_struct guard
    refs: Rve27/KernelSU-Next@4a3350a

--- drivers/kernelsu/sucompat.c
+++ drivers/kernelsu/sucompat.c
@@ -218,12 +218,7 @@ int ksu_handle_devpts(struct inode *inode)
 		return 0;
 
 	if (ksu_devpts_sid) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
 		struct inode_security_struct *sec = selinux_inode(inode);
-#else
-		struct inode_security_struct *sec =
-			(struct inode_security_struct *)inode->i_security;
-#endif
 		if (sec) {
 			sec->sid = ksu_devpts_sid;
 		}
