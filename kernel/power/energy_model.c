456cf4126ed61938acad13c68457f2bd1284a3cf
TIMISONG-dev <dima.pilaer@gmail.com>
2026-01-29 20:23:12 +0300

    Revert "power: em: correct increasing freq/power ratio"
    This reverts commit c208338efc650057c49cd5c91ff0cdf43d84d194.

--- kernel/power/energy_model.c
+++ kernel/power/energy_model.c
@@ -153,10 +153,6 @@ static struct em_perf_domain *em_create_pd(cpumask_t *span, int nr_states,
 
 		table[i].cost = div64_u64(fmax * power_res,
 					  table[i].frequency);
-		if (i > 0 && (table[i].cost < table[i - 1].cost) &&
-				(table[i].power > table[i - 1].power)) {
-			table[i].cost = table[i - 1].cost;
-		}
 	}
 
 	pd->table = table;
