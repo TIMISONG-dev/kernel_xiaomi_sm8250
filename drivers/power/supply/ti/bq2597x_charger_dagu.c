5679ef57bc98270c2dbea9e0b976fbb8fb2bacc5
Cloud_Yun <1770669041@qq.com>
2026-02-04 13:59:10 +0900

    power: fix dagu build error
    Signed-off-by: Cloud_Yun <1770669041@qq.com>

--- drivers/power/supply/ti/bq2597x_charger_dagu.c
+++ drivers/power/supply/ti/bq2597x_charger_dagu.c
@@ -1103,10 +1103,8 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel, int *result)
 		*result = t;
 		/* vbat need calibration read by NU2105 */
 		if (channel == ADC_VBAT) {
-			kernel_neon_begin();
-			t = t * (1 + 1.803 * 0.001);
+			t = t * (1 + 1803 / 1000000);
 			*result = t;
-			kernel_neon_end();
 		}
 	} else {
 		ret = bq2597x_read_word(bq, ADC_REG_BASE + (channel << 1),
