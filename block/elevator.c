e4dcc9ba48bdee3e3ab846fb9f1cb155e7c43088
Cloud_Yun <1770669041@qq.com>
2026-02-05 22:46:00 +0900

    block: set default io scheduler to mq-deadline
    Signed-off-by: Cloud_Yun <1770669041@qq.com>

--- block/elevator.c
+++ block/elevator.c
@@ -964,15 +964,11 @@ int elevator_init_mq(struct request_queue *q)
 
 	if (unlikely(q->elevator))
 		goto out;
-	if (IS_ENABLED(CONFIG_IOSCHED_BFQ)) {
-		e = elevator_get(q, "bfq", false);
-		if (!e)
-			goto out;
-	} else {
-		e = elevator_get(q, "mq-deadline", false);
-		if (!e)
-			goto out;
-	}
+
+	e = elevator_get(q, "mq-deadline", false);
+	if (!e)
+		goto out;
+
 	err = blk_mq_init_sched(q, e);
 	if (err)
 		elevator_put(e);
