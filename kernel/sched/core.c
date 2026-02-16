26ff4c25fe1f223d461ba8c47cf52b370447f69c
Xie Yuanbin <qq570070308@gmail.com>
2026-02-17 00:49:49 +0800

    sched: Make raw_spin_rq_unlock() inline
    raw_spin_rq_unlock() is short, and is called in some hot code paths
    such as finish_lock_switch.
    
    Make raw_spin_rq_unlock() inline to optimize performance.
    
    Signed-off-by: Xie Yuanbin <qq570070308@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260216164950.147617-3-qq570070308@gmail.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -407,11 +407,6 @@ bool raw_spin_rq_trylock(struct rq *rq)
 	}
 }
 
-void raw_spin_rq_unlock(struct rq *rq)
-{
-	raw_spin_unlock(rq_lockp(rq));
-}
-
 #ifdef CONFIG_SMP
 /*
  * double_rq_lock - safely lock two runqueues
