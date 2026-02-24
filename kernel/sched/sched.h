dc4374383cda1cfecb40f8f6602b87d197af91df
Peter Zijlstra (Intel) <peterz@infradead.org>
2026-02-24 17:35:22 +0100

    sched/fair: Simplify hrtick_update()
    hrtick_update() was needed when the slice depended on nr_running, all that
    code is gone. All that remains is starting the hrtick when nr_running
    becomes more than 1.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260224163428.866374835@kernel.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2564,6 +2564,10 @@ static inline int hrtick_enabled_dl(struct rq *rq)
 }
 
 void hrtick_start(struct rq *rq, u64 delay);
+static inline bool hrtick_active(struct rq *rq)
+{
+	return hrtimer_active(&rq->hrtick_timer);
+}
 
 #else
 
