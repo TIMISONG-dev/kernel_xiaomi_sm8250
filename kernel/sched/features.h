21653a15ea5040c706c5c35e81f6b20dcb1d9d99
TIMISONG-dev <dima.pilaer@gmail.com>
2026-03-08 23:05:21 +0300

    fix! sched/fair: Increase weight bits for avg_vruntime

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -57,7 +57,7 @@ SCHED_FEAT(CACHE_HOT_BUDDY, false)
 SCHED_FEAT(DELAY_DEQUEUE, true)
 SCHED_FEAT(DELAY_ZERO, true)
 
-#define SCHED_FEAT_PARANOID_AVG 0
+SCHED_FEAT(PARANOID_AVG, false)
 
 /*
  * Allow wakeup-time preemption of the current task:
