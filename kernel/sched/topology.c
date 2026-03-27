1fe3bd8baf1915e90de18235008aa289a7511629
Vincent Guittot <vincent.guittot@linaro.org>
2026-03-27 14:20:13 +0100

    sched/fair: Use sched_energy_enabled()
    Use helper sched_energy_enabled() everywhere we want to test if EAS is
    enabled instead of mixing sched_energy_enabled() and direct call to
    static_branch_unlikely().
    
    No functional change
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260327132013.2800517-1-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -195,15 +195,14 @@ bool sched_energy_update;
 int sched_energy_aware_handler(struct ctl_table *table, int write,
 		void *buffer, size_t *lenp, loff_t *ppos)
 {
-	int ret, state;
+	int ret;
 
 	if (write && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
 
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (!ret && write) {
-		state = static_branch_unlikely(&sched_energy_present);
-		if (state != sysctl_sched_energy_aware) {
+		if (sysctl_sched_energy_aware != sched_energy_enabled()) {
 			mutex_lock(&sched_energy_mutex);
 			sched_energy_update = 1;
 			rebuild_sched_domains();
@@ -286,11 +285,11 @@ static void destroy_perf_domain_rcu(struct rcu_head *rp)
 
 static void sched_energy_set(bool has_eas)
 {
-	if (!has_eas && static_branch_unlikely(&sched_energy_present)) {
+	if (!has_eas && sched_energy_enabled()) {
 		if (sched_debug())
 			pr_info("%s: stopping EAS\n", __func__);
 		static_branch_disable_cpuslocked(&sched_energy_present);
-	} else if (has_eas && !static_branch_unlikely(&sched_energy_present)) {
+	} else if (has_eas && !sched_energy_enabled()) {
 		if (sched_debug())
 			pr_info("%s: starting EAS\n", __func__);
 		static_branch_enable_cpuslocked(&sched_energy_present);
