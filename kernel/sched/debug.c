ee49ecf7a520bfc440f30a0862dc83294f77229a
Peter Zijlstra <peterz@infradead.org>
2026-04-01 15:20:21 +0200

    sched/debug: Fix avg_vruntime() usage
    John reported that stress-ng-yield could make his machine unhappy and
    managed to bisect it to commit b3d99f43c72b ("sched/fair: Fix
    zero_vruntime tracking").
    
    The commit in question changes avg_vruntime() from a function that is
    a pure reader, to a function that updates variables. This turns an
    unlocked sched/debug usage of this function from a minor mistake into
    a data corruptor.
    
    Fixes: af4cf40470c2 ("sched/fair: Add cfs_rq::avg_vruntime")
    Fixes: b3d99f43c72b ("sched/fair: Fix zero_vruntime tracking")
    Reported-by: John Stultz <jstultz@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Tested-by: John Stultz <jstultz@google.com>
    Link: https://patch.msgid.link/20260401132355.196370805@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -510,6 +510,7 @@ void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 {
 	s64 left_vruntime = -1, right_vruntime = -1, left_deadline = -1, spread;
 	s64 zero_vruntime = -1, sum_w_vruntime = -1;
+	u64 avruntime;
 	struct sched_entity *last, *first, *root;
 	struct rq *rq = cpu_rq(cpu);
 	unsigned int sum_shift;
@@ -538,6 +539,7 @@ void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 		right_vruntime = last->vruntime;
 	zero_vruntime = cfs_rq->zero_vruntime;
 	sum_w_vruntime = cfs_rq->sum_w_vruntime;
+	avruntime = avg_vruntime(cfs_rq);
 	sum_weight = cfs_rq->sum_weight;
 	sum_shift = cfs_rq->sum_shift;
 	raw_spin_rq_unlock_irqrestore(rq, flags);
@@ -554,7 +556,7 @@ void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq)
 		   sum_weight);
 	SEQ_printf(m, "  .%-30s: %u\n", "sum_shift", sum_shift);
 	SEQ_printf(m, "  .%-30s: %Ld.%06ld\n", "avg_vruntime",
-			SPLIT_NS(avg_vruntime(cfs_rq)));
+			SPLIT_NS(avruntime));
 	SEQ_printf(m, "  .%-30s: %Ld.%06ld\n", "right_vruntime",
 			SPLIT_NS(right_vruntime));
 	spread = right_vruntime - left_vruntime;
