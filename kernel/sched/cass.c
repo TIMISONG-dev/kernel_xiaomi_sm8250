7913a302ca82a51df32f64ef03c0a8e613fba35c
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-04-13 21:10:28 +0600

    sched/cass: Use the new fits_capacity() logic
    We have moved away from hardcoded magic number in fits_capacity() for a more
    robust calculation [1]. The parameters of fits_capacity() has changed. Update and
    use the new logic.
    
    [1] 520c9252dc99 ("BACKPORT: sched/fair: Remove magic hardcoded margin in fits_capacity()")
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -98,8 +98,8 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 		goto done;
 
 	/* Prefer the CPU that fits the task */
-	if (cass_cmp(fits_capacity(p_util, a->cap_max),
-		     fits_capacity(p_util, b->cap_max)))
+	if (cass_cmp(fits_capacity(p_util, a->cpu),
+		     fits_capacity(p_util, b->cpu)))
 		goto done;
 
 	/* Prefer the CPU with lower relative utilization */
