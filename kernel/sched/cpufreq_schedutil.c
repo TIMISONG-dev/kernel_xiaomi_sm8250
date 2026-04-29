20c2713d5a2a511944f302b107a9ff8c0ad998d3
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-04-30 02:15:28 +0600

    cpufreq: schedutil: Remove redundant dvfs_update_delay per-cpu usage
    We know that our ratelimit will always stay at 2000us. So, make
    rate_limit_us_store() no-op so that nobody can mess with it.
    
    max(TICK_USEC, 2000) evaluates to TICK_USEC only when CONFIG_HZ is 100, 250 and
    300.
    
    But for CONFIG_HZ=1000,
    TICK_USEC = (1000000 + (1000 / 2)) / 1000 = 1000
    
    max(TICK_USEC, 2000) = max(1000, 2000) = 2000.
    
    Use compile-time macros and conditionally handle this. We currently use
    CONFIG_HZ=300, so the max() is a burden only in this hot path. Micro-optimize
    and get rid of it wherever possible.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -309,14 +309,19 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
  * Precompute the DVFS headroom lookup table when delay is one scheduler
  * tick rate.
  */
+#define SUGOV_RATE_LIMIT_US 2000ULL
+#ifdef CONFIG_HZ_1000
+#define SUGOV_DELAY_US SUGOV_RATE_LIMIT_US
+#else
+#define SUGOV_DELAY_US TICK_USEC
+#endif
+
 static void sugov_build_dvfs_headroom_lut(struct sugov_policy *sg_policy)
 {
-	unsigned int cpu = cpumask_first(sg_policy->policy->cpus);
 	unsigned int i;
 	u64 delay;
 
-	delay = TICK_USEC;
-	delay = max_t(u64, delay, per_cpu(dvfs_update_delay, cpu));
+	delay = SUGOV_DELAY_US;
 
 	sg_policy->dvfs_headroom_lut_delay = delay;
 
@@ -334,7 +339,7 @@ static void sugov_build_dvfs_headroom_lut(struct sugov_policy *sg_policy)
  *
  * This function provides enough headroom to provide adequate performance
  * assuming the CPU continues to be busy. This headroom is based on the
- * dvfs_update_delay of the cpufreq governor or min(curr.se.slice, TICK_US),
+ * rate_limit_us of the cpufreq governor or min(curr.se.slice, TICK_US),
  * whichever is higher.
  *
  * XXX: Should we provide headroom when the util is decaying?
@@ -354,11 +359,15 @@ static inline unsigned long sugov_apply_dvfs_headroom(unsigned long util, int cp
 	 * What is the possible worst case scenario for updating util_avg, ctx
 	 * switch or TICK?
 	 */
-	if (rq->cfs.h_nr_queued > 1)
-		delay = min_t(u64, rq->curr->se.slice/1000, TICK_USEC);
-	else
+	if (rq->cfs.h_nr_queued > 1) {
+		delay = min_t(u64, rq->curr->se.slice / 1000, TICK_USEC);
+		delay = max(delay, SUGOV_RATE_LIMIT_US);
+	} else {
 		delay = TICK_USEC;
-	delay = max(delay, per_cpu(dvfs_update_delay, cpu));
+#ifdef CONFIG_HZ_1000
+		delay = max(delay, SUGOV_RATE_LIMIT_US);
+#endif
+	}
 
 	if (likely(delay == sg_policy->dvfs_headroom_lut_delay)) {
 		approx = sg_policy->dvfs_headroom_lut[util];
@@ -762,25 +771,6 @@ static ssize_t rate_limit_us_show(struct gov_attr_set *attr_set, char *buf)
 static ssize_t
 rate_limit_us_store(struct gov_attr_set *attr_set, const char *buf, size_t count)
 {
-	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
-	struct sugov_policy *sg_policy;
-	unsigned int rate_limit_us;
-	int cpu;
-
-	if (kstrtouint(buf, 10, &rate_limit_us))
-		return -EINVAL;
-
-	tunables->rate_limit_us = rate_limit_us;
-
-	list_for_each_entry(sg_policy, &attr_set->policy_list, tunables_hook) {
-		sg_policy->freq_update_delay_ns = rate_limit_us * NSEC_PER_USEC;
-
-		for_each_cpu(cpu, sg_policy->policy->cpus)
-			per_cpu(dvfs_update_delay, cpu) = rate_limit_us;
-
-		sugov_build_dvfs_headroom_lut(sg_policy);
-	}
-
 	return count;
 }
 
@@ -974,6 +964,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		sg_policy->tunables = global_tunables;
 
 		gov_attr_set_get(&global_tunables->attr_set, &sg_policy->tunables_hook);
+		sugov_build_dvfs_headroom_lut(sg_policy);
 		goto out;
 	}
 
@@ -989,6 +980,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->rate_limit_us = 2000;
 	tunables->response_time_ms = sugov_calc_freq_response_ms(sg_policy);
 	sugov_update_response_time_mult(sg_policy);
+	sugov_build_dvfs_headroom_lut(sg_policy);
 
 	ret = kobject_init_and_add(&tunables->attr_set.kobj, &sugov_tunables_ktype,
 				   get_governor_parent_kobj(policy), "%s",
@@ -1068,12 +1060,8 @@ else
 		sg_cpu->cpu = cpu;
 		sg_cpu->sg_policy = sg_policy;
 
-		per_cpu(dvfs_update_delay, cpu) = sg_policy->tunables->rate_limit_us;
-
 		cpufreq_add_update_util_hook(cpu, &sg_cpu->update_util, uu);
 	}
-
-	sugov_build_dvfs_headroom_lut(sg_policy);
 
 	return 0;
 }
