4d6f39f89582daf2cdac9243ccd1ac754cd8e38a
TIMISONG-dev <dima.pilaer@gmail.com>
2026-06-05 01:27:05 +0300

    fix! BACKPORT: mmap locking API: rename mmap_sem to mmap_lock

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1683,7 +1683,7 @@ static inline bool can_reclaim(short before_reclaim_adj,
 	if (running_state == false ||
 			fatal_signal_pending(task) ||
 			task->flags & PF_EXITING ||
-			!list_empty(&mm->mmap_sem.wait_list)) {
+			!list_empty(&mm->mmap_lock.wait_list)) {
 		pr_info("stop reclaim: force\n");
 
 		return false;
