4dde3bff96336dec06772724244ad3c0a702ab94
TIMISONG-dev <dima.pilaer@gmail.com>
2026-06-06 22:30:17 +0300

    Revert "UPSTREAM: xarray: Add definition of struct xarray"
    This reverts commit 1dd6a092a678457508290d4103a4f89ae52076d1.

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -30,9 +30,6 @@
 #include <linux/types.h>
 #include <linux/xarray.h>
 
-/* Keep unconverted code working */
-#define radix_tree_root		xarray
-
 /*
  * The bottom two bits of the slot determine how the remaining bits in the
  * slot are interpreted:
@@ -95,21 +92,36 @@ struct radix_tree_node {
 	unsigned long	tags[RADIX_TREE_MAX_TAGS][RADIX_TREE_TAG_LONGS];
 };
 
-/* The IDR tag is stored in the low bits of xa_flags */
+/* The IDR tag is stored in the low bits of the GFP flags */
 #define ROOT_IS_IDR	((__force gfp_t)4)
-/* The top bits of xa_flags are used to store the root tags */
+/* The top bits of gfp_mask are used to store the root tags */
 #define ROOT_TAG_SHIFT	(__GFP_BITS_SHIFT)
 
-#define RADIX_TREE_INIT(name, mask)	XARRAY_INIT(name, mask)
+struct radix_tree_root {
+	spinlock_t		xa_lock;
+	gfp_t			gfp_mask;
+	struct radix_tree_node	__rcu *rnode;
+};
+
+#define RADIX_TREE_INIT(name, mask)	{				\
+	.xa_lock = __SPIN_LOCK_UNLOCKED(name.xa_lock),			\
+	.gfp_mask = (mask),						\
+	.rnode = NULL,							\
+}
 
 #define RADIX_TREE(name, mask) \
 	struct radix_tree_root name = RADIX_TREE_INIT(name, mask)
 
-#define INIT_RADIX_TREE(root, mask) xa_init_flags(root, mask)
+#define INIT_RADIX_TREE(root, mask)					\
+do {									\
+	spin_lock_init(&(root)->xa_lock);				\
+	(root)->gfp_mask = (mask);					\
+	(root)->rnode = NULL;						\
+} while (0)
 
 static inline bool radix_tree_empty(const struct radix_tree_root *root)
 {
-	return root->xa_head == NULL;
+	return root->rnode == NULL;
 }
 
 /**
