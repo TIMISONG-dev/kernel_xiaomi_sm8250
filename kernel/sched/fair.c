33bfceb13662a4f077e79b872a605c1aab45ef60
Vincent Guittot <vincent.guittot@linaro.org>
2026-06-24 17:12:28 +0200

    BACKPORT: sched/eevdf: Always update slice protection
    Even if p will not preempt current, it modifies the avg_vruntime and
    possibly the min slice. Make sure to update the slice protection with the
    updated figures. As an example, Batch and Sched Idle tasks can otherwise
    get a larger lag than their slice and finaly delay the scheduling of a
    normal task, which deadline will be a later.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://patch.msgid.link/20260624151229.1710703-6-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8406,11 +8406,11 @@ static void check_preempt_wakeup_fair(struct rq *rq, struct task_struct *p, int 
 	if (cse_is_idle && !pse_is_idle)
 		goto preempt;
 
-	if (cse_is_idle != pse_is_idle)
-		return;
-
 	cfs_rq = cfs_rq_of(se);
 	update_curr(cfs_rq);
+
+	if (cse_is_idle != pse_is_idle)
+		goto update;
 
 	/*
 	 * If @p has a shorter slice than current and @p is eligible, override
@@ -8428,7 +8428,7 @@ static void check_preempt_wakeup_fair(struct rq *rq, struct task_struct *p, int 
 	 * EEVDF to forcibly queue an ineligible task.
 	 */
 	if ((wake_flags & WF_FORK) || pse->sched_delayed)
-		return;
+		goto update;
 
 	/* Prefer picking wakee soon if appropriate. */
 	if (sched_feat(NEXT_BUDDY) && set_preempt_buddy(cfs_rq, pse)) {
@@ -8472,7 +8472,7 @@ pick:
 	 */
 	if (preempt_action == PREEMPT_WAKEUP_SHORT && entity_eligible(cfs_rq, pse))
 		goto preempt;
-
+update:
 	if (sched_feat(RUN_TO_PARITY))
 		update_protect_slice(cfs_rq, se);
 
