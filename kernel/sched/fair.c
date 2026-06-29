fa17fc15a98c2e48ff9be744726df445307ce405
Zhan Xusheng <zhanxusheng1024@gmail.com>
2026-06-29 11:02:00 +0800

    sched/fair: Fix stale comments referring to removed CFS concepts
    A few comments still describe the pre-EEVDF CFS world:
    
     - sysctl_sched_base_slice is documented as "Minimal preemption
       granularity for CPU-bound tasks".  That was the wording of the old
       sysctl_sched_min_granularity, renamed in commit e4ec3318a17f
       ("sched/debug: Rename sysctl_sched_min_granularity to
       sysctl_sched_base_slice").  Under EEVDF it is the default base time
       slice / request size (r_i) used to compute the virtual deadline, as
       documented in update_deadline().
    
     - Two comments still mention sched_slice(), which was removed when the
       fair class committed to EEVDF in commit 5e963f2bd465 ("sched/fair:
       Commit to EEVDF").  The dequeue-path comment should simply refer to
       the task's slice (se->slice); the forced-idle comment describes the
       slice accounting now performed by __entity_slice_used(), which is the
       function actually used right below it.
    
    No functional changes.
    
    [ mingo: Ported to a more recent scheduler base ]
    
    Signed-off-by: Zhan Xusheng <zhanxusheng@xiaomi.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://patch.msgid.link/20260629030200.3165589-1-zhanxusheng@xiaomi.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -38,7 +38,10 @@
 enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
 
 /*
- * Minimal preemption granularity for CPU-bound tasks:
+ * Default base time slice (request size r_i) for SCHED_NORMAL/SCHED_BATCH:
+ *
+ * Under EEVDF this is the request size used to compute the virtual
+ * deadline; see update_deadline().
  *
  * (default: 0.70 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
@@ -12599,7 +12602,7 @@ static inline void task_tick_core(struct rq *rq, struct task_struct *curr)
 	 * if the sibling is forced idle, then trigger schedule to
 	 * give forced idle task a chance.
 	 *
-	 * sched_slice() considers only this active rq and it gets the
+	 * __entity_slice_used() considers only this active rq and it gets the
 	 * whole slice. But during force idle, we have siblings acting
 	 * like a single runqueue and hence we need to consider runnable
 	 * tasks on this CPU and the forced idle CPU. Ideally, we should
