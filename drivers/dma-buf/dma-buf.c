230b62673818c3e04f8bc0008fda5b2bc5ca129d
TIMISONG-dev <dima.pilaer@gmail.com>
2026-08-02 15:38:38 +0300

    Revert "UPSTREAM: dmabuf: fix NULL pointer dereference in dma_buf_release()"
    This reverts commit 832119d57ca2803c22cee7bc68ad20780ae80082.

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -105,8 +105,6 @@ static void dma_buf_release(struct dentry *dentry)
 	int dtor_ret = 0;
 
 	dmabuf = dentry->d_fsdata;
-	if (unlikely(!dmabuf))
-		return;
 
 	spin_lock(&dentry->d_lock);
 	dentry->d_fsdata = NULL;
