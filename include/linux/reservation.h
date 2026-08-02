bdf3c06660748adf1ee48ed0b4e2f44eb9582920
TIMISONG-dev <dima.pilaer@gmail.com>
2026-08-02 15:39:45 +0300

    Revert "UPSTREAM: dma-buf: clarify locking documentation for reservation_object_get_excl"
    This reverts commit c53a29cecdfcafd32437ce778ba30f3700fadb05.

--- include/linux/reservation.h
+++ include/linux/reservation.h
@@ -179,8 +179,7 @@ reservation_object_unlock(struct reservation_object *obj)
  * @obj: the reservation object
  *
  * Returns the exclusive fence (if any).  Does NOT take a
- * reference. Writers must hold obj->lock, readers may only
- * hold a RCU read side lock.
+ * reference.  The obj->lock must be held.
  *
  * RETURNS
  * The exclusive fence or NULL
