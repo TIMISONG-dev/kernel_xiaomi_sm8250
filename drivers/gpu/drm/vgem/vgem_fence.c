c942d71f37a79991ca8120fd49290c4435a64daa
TIMISONG-dev <dima.pilaer@gmail.com>
2026-08-02 15:39:52 +0300

    Revert "UPSTREAM: dma-buf: make fence sequence numbers 64 bit v2"
    This reverts commit ad4af6dde9b5ba769e512038ae38ca718da1ac19.

--- drivers/gpu/drm/vgem/vgem_fence.c
+++ drivers/gpu/drm/vgem/vgem_fence.c
@@ -63,13 +63,13 @@ static void vgem_fence_release(struct dma_fence *base)
 
 static void vgem_fence_value_str(struct dma_fence *fence, char *str, int size)
 {
-	snprintf(str, size, "%llu", fence->seqno);
+	snprintf(str, size, "%u", fence->seqno);
 }
 
 static void vgem_fence_timeline_value_str(struct dma_fence *fence, char *str,
 					  int size)
 {
-	snprintf(str, size, "%llu",
+	snprintf(str, size, "%u",
 		 dma_fence_is_signaled(fence) ? fence->seqno : 0);
 }
 
