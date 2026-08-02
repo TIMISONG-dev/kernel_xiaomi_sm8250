5c5431017cd42b7c42f482046a2f36f810bb8fe9
TIMISONG-dev <dima.pilaer@gmail.com>
2026-08-02 15:39:49 +0300

    Revert "UPSTREAM: dma-buf: Make mmap callback actually optional"
    This reverts commit b2a6cbfe49f6da10b3021849754704fd17803525.

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -188,10 +188,6 @@ static int dma_buf_mmap_internal(struct file *file, struct vm_area_struct *vma)
 
 	dmabuf = file->private_data;
 
-	/* check if buffer supports mmap */
-	if (!dmabuf->ops->mmap)
-		return -EINVAL;
-
 	/* check for overflowing the buffer's size */
 	if (vma->vm_pgoff + vma_pages(vma) >
 	    dmabuf->size >> PAGE_SHIFT)
@@ -604,7 +600,8 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 			  || !exp_info->ops
 			  || !exp_info->ops->map_dma_buf
 			  || !exp_info->ops->unmap_dma_buf
-			  || !exp_info->ops->release)) {
+			  || !exp_info->ops->release
+			  || !exp_info->ops->mmap)) {
 		return ERR_PTR(-EINVAL);
 	}
 
@@ -1185,10 +1182,6 @@ int dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma,
 		 unsigned long pgoff)
 {
 	if (WARN_ON(!dmabuf || !vma))
-		return -EINVAL;
-
-	/* check if buffer supports mmap */
-	if (!dmabuf->ops->mmap)
 		return -EINVAL;
 
 	/* check for offset overflow */
