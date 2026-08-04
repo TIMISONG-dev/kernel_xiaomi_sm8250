e52d844fc97a50609948dc4d7c68aa59720ff09a
Boqun Feng <boqun@kernel.org>
2026-08-04 09:14:30 -0700

    sched: Remove the unused preempt_offset parameter of __cant_sleep()
    The preempt_offset is always 0 in all the callsites of __cant_sleep(),
    hence remove it. It also allows us to clear up the code a bit by
    no longer using a "preempt_count() > .." comparison.
    
    Signed-off-by: Boqun Feng <boqun@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260804161447.84806-9-boqun@kernel.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9151,7 +9151,7 @@ void __might_resched(const char *file, int line, int preempt_offset)
 }
 EXPORT_SYMBOL(__might_resched);
 
-void __cant_sleep(const char *file, int line, int preempt_offset)
+void __cant_sleep(const char *file, int line)
 {
 	static unsigned long prev_jiffy;
 
@@ -9161,7 +9161,7 @@ void __cant_sleep(const char *file, int line, int preempt_offset)
 	if (!IS_ENABLED(CONFIG_PREEMPT_COUNT))
 		return;
 
-	if (preempt_count() > preempt_offset)
+	if (preempt_count())
 		return;
 
 	if (time_before(jiffies, prev_jiffy + HZ) && prev_jiffy)
