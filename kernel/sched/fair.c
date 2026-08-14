abd553eaf905c1d14761a7726c69b7ad5608d6e0
Vincent Guittot <vincent.guittot@linaro.org>
2026-08-14 15:52:41 +0200

    sched: Update time before requeueing delayed entities
    In order to compute the right lag, it is required to update time to 'now'.
    Without this, the delayed entity might appear younger than it really is and
    receive less compensation for having waited.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6578,6 +6578,8 @@ requeue_delayed_entity(struct sched_entity *se)
 	SCHED_WARN_ON(!se->sched_delayed);
 	SCHED_WARN_ON(!se->on_rq);
 
+	update_curr(cfs_rq);
+
 	if (update_entity_lag(cfs_rq, se)) {
 		cfs_rq->nr_queued--;
 		if (se != cfs_rq->curr)
